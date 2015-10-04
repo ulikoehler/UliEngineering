43e036394da55b98e4858041a9263f2392e4ef02
Uli Köhler <ulikoehler@online.de>
2015-10-04 23:38:35 +0200

    More noise density functions

--- UliEngineering/NoiseDensity.py
+++ UliEngineering/NoiseDensity.py
@@ -7,20 +7,35 @@ from .Resistors import *
 from .EngineerIO import *
 import numpy as np
 
-def noise(density, bandwith):
+def actualNoise(density, bandwith):
     """
     Compute the actual noise given:
      - A noise density in x/√Hz where x is any unit
      - A bandwith in ΔHz
 
-    >>> noise("100 mV", "100 Hz")
-    1.0
+    >>> formatValue(actualNoise("100 µV", "100 Hz"), "V")
+    '1.00 mV'
     """
     if isinstance(density, str):
         density, _ = normalizeEngineerInput(density)
     if isinstance(bandwith, str):
         bandwith, _ = normalizeEngineerInput(bandwith)
     return np.sqrt(bandwith) * density
+
+def noiseDensity(actual_noise, bandwith):
+    """
+    Compute the noise density given:
+     - A noise density in x/√Hz where x is any unit
+     - A bandwith in ΔHz
+
+    >>> formatValue(noiseDensity("1.0 mV", "100 Hz"), "V/√Hz")
+    '100 μV/√Hz'
+    """
+    if isinstance(actual_noise, str):
+        actual_noise, _ = normalizeEngineerInput(actual_noise)
+    if isinstance(bandwith, str):
+        bandwith, _ = normalizeEngineerInput(bandwith)
+    return actual_noise / np.sqrt(bandwith)
 
 if __name__ == "__main__":
     import doctest
