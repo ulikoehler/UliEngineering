839cdf523a7bb807cd73a009091ff8aa54c36aa5
Uli Köhler <ulikoehler@online.de>
2015-10-07 16:54:35 +0200

    Added capacitor functions

---
+++ UliEngineering/Capacitors.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+import scipy.constants
+from .Resistors import *
+from .EngineerIO import *
+import math
+import numpy as np
+
+def capacitorEnergy(capacitance, voltage):
+    """
+    Compute the total energy stored in a capacitor given:
+    - The capacitance in farads
+    - The voltage the capacitor is charged to
+    The energy is returned as joules.
+
+    >>> capacitorEnergy("1.5 F", "5.0 V")
+    18.75
+    >>> capacitorEnergy("1.5 F", "0.0 V")
+    0.0
+    """
+    if isinstance(capacitance, str):
+        capacitance, _ = normalizeEngineerInput(capacitance)
+    if isinstance(voltage, str):
+        voltage, _ = normalizeEngineerInput(voltage)
+    return 0.5 * capacitance * np.square(voltage)
+
+def capacitorCharge(capacitance, voltage):
+    """
+    Compute the total charge stored in a capacitor given:
+    - The capacitance in farads
+    - The voltage the capacitor is charged to
+    The energy is returned in coulombs.
+
+    >>> capacitorCharge("1.5 F", "5.0 V")
+    7.5
+    >>> capacitorCharge("1.5 F", "0.0 V")
+    0.0
+    """
+    if isinstance(capacitance, str):
+        capacitance, _ = normalizeEngineerInput(capacitance)
+    if isinstance(voltage, str):
+        voltage, _ = normalizeEngineerInput(voltage)
+    return capacitance * voltage
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
+    # Usage example
+    print(formatValue(capacitorEnergy("100 mF", "1.2 V"), "J"))
