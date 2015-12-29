70aa9ef7343fd4ba9293780058490435d104bc46
Uli Köhler <ulikoehler@online.de>
2015-12-29 21:40:46 +0100

    Added performCopy option to fixedSizeChunkGenerator

--- UliEngineering/DataScience/Chunks.py
+++ UliEngineering/DataScience/Chunks.py
@@ -18,21 +18,28 @@ def evaluateGeneratorFunction(tp, as_list=False):
     return list(gen) if as_list else gen
 
 
-def __fixedSizeChunkGenerator(ofsTable, chunksize, y, i):
+def __fixedSizeChunkGenerator(ofsTable, chunksize, y, perform_copy, i):
     """Worker for fixedSizeChunkGenerator()"""
     yofs = ofsTable[i]
-    return y[yofs:yofs + chunksize].copy()
+    yslice = y[yofs:yofs + chunksize]
+    return yslice.copy() if perform_copy else yslice
 
-def fixedSizeChunkGenerator(y, chunksize, shiftsize):
+def fixedSizeChunkGenerator(y, chunksize, shiftsize, perform_copy=True):
     """
     A chunk-generating function that can be used for parallelFFTSum().
     Generates only full chunks with variable chunk / shift size.
 
-    Returns copies of chunks, so this can be safely used for non-writable datasets.
+    If perform_copy=False, the chunk is not copied in the generator function.
+    Functions like parallelFFTSum() modify the data which might lead to
+    undesired overwriting of data. However, setting perform_copy=False might
+    improve the performance significantly if the downstream function does
+    not require copies.
+
     This is a lazy function, it generates copies only on-demand.
 
-    Returns (n, g) where g is a unary generator function and n is the number of chunks.
+    Returns (n, g) where g is a unary generator function (which takes the chunk
+        number as an argument) and n is the number of chunks.
     """
     # Precompute offset table
     offsets = [ofs for ofs in range(0, y.shape[0] - (chunksize - 1), shiftsize)]
-    return len(offsets), functools.partial(__fixedSizeChunkGenerator, offsets, chunksize, y)
+    return len(offsets), functools.partial(__fixedSizeChunkGenerator, offsets, chunksize, y, perform_copy)
--- tests/DataScience/TestChunks.py
+++ tests/DataScience/TestChunks.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 from nose.tools import assert_equal, assert_true, raises
 import numpy as np
-from numpy.testing import assert_array_equal
+from numpy.testing import assert_array_equal, assert_array_less
 from UliEngineering.DataScience.Chunks import fixedSizeChunkGenerator, evaluateGeneratorFunction
 
 class TestChunkGeneration(object):
@@ -11,12 +11,30 @@ class TestChunkGeneration(object):
     def test_fixedSizeChunkGenerator(self):
         # Odd-sized array
         vals = evaluateGeneratorFunction(fixedSizeChunkGenerator(self.data1, 3, 3), as_list=True)
-        expected = np.asarray([[1, 2, 3],  [4, 5, 6],  [7, 8, 9]])
+        expected = np.asarray([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
         assert_array_equal(vals, expected)
         # Even-sized array
         vals = evaluateGeneratorFunction(fixedSizeChunkGenerator(self.data2, 3, 3), as_list=True)
         expected = np.asarray([[1, 2, 3], [4, 5, 6], [7, 8, 9], [10, 11, 12]])
         assert_array_equal(vals, expected)
+        # Array which is too long
+        vals = evaluateGeneratorFunction(fixedSizeChunkGenerator(self.data2, 25, 3), as_list=True)
+        expected = np.asarray([])
+        assert_array_equal(vals, expected)
+    def test_fixedSizeChunkGenerator_perform_copy(self):
+        d1 = self.data1.copy()
+        d2 = self.data1.copy()
+        # perform_copy=True should be the default.
+        vals = evaluateGeneratorFunction(fixedSizeChunkGenerator(d1, 3, 10), as_list=True)
+        vals[0][0] = -1000  # Change value ...
+        assert_array_equal(d1, d2)  # ... should not have any effect
+        # Do the same without copy
+        vals = evaluateGeneratorFunction(fixedSizeChunkGenerator(d1, 3, 10, perform_copy=False), as_list=True)
+        vals[0][0] = -1000  # Change value ...
+        d2[0] = -1000  # Should make d2 equal to d1
+        assert_array_equal(d1, d2)  # ... should have an effect
+
+
     @raises
     def test_fixedSizeChunkGenerator_invalid1(self):
         fixedSizeChunkGenerator(None, 3, 3)
