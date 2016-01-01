d28771d3c89586ddc1f61ef3fe436c282a2965b2
Uli Köhler <ulikoehler@online.de>
2016-01-01 18:09:25 +0100

    Move OpAmp to Electronics module & add unit test

--- UliEngineering/Electronics/OpAmp.py
+++ UliEngineering/Electronics/OpAmp.py
@@ -1,11 +1,18 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Utilities for operational amplifier calculations
+Utilities for operational amplifier calculations.
+
+Usage example:
+>>> from UliEngineering.Electronics.OpAmp import summing_amplifier_noninv
+>>> # Example: sum 2.5V and 0.5V with a total sum-referred gain of 1.0
+>>> formatValue(summing_amplifier_noninv(
+        "2.5V", "500mV", "1kΩ", "1kΩ", "1kΩ", "1kΩ"), "V"))
+
 """
 import itertools
 import math
-from .EngineerIO import *
+from UliEngineering.EngineerIO import normalizeEngineerInputIfStr
 
 def summing_amplifier_noninv(v1, v2, r1, r2, rfb1, rfb2):
     """
@@ -22,8 +29,3 @@ def summing_amplifier_noninv(v1, v2, r1, r2, rfb1, rfb2):
     rfb1, _ = normalizeEngineerInputIfStr(rfb1)
     rfb2, _ = normalizeEngineerInputIfStr(rfb2)
     return (1.0 + rfb2 / rfb1) * (v1 * (r2 / (r1 + r2)) + v2 * (r1 / (r1 + r2)))
-
-if __name__ == "__main__":
-    # Example: sum 2.5V and 0.5V with a total sum-referred gain of 1.0
-    print(formatValue(summing_amplifier_noninv(
-        "2.5V", "500mV", "1kΩ", "1kΩ", "1kΩ", "1kΩ"), "V"))
---
+++ tests/Electronics/TestOpAmp.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+from numpy.testing import assert_approx_equal
+from nose.tools import assert_equal
+from UliEngineering.Electronics.OpAmp import *
+from UliEngineering.EngineerIO import formatValue
+
+class TestOpAmp(object):
+    def test_summing_amplifier_noninv(self):
+        v = summing_amplifier_noninv("2.5V", "500mV", "1kΩ", "1kΩ", "1kΩ", "1kΩ")
+        assert_approx_equal(v, 3.0)
+        v = summing_amplifier_noninv(2.5, 0.5, 1e3, 1e3, 300, 1e3)
+        assert_approx_equal(v, 6.5)
