36dff5821ef42dfc68cd7c22a2bd822f9c673657
Uli Köhler <ulikoehler@online.de>
2016-01-03 19:24:08 +0100

    Added capacitors test with 100% coverage

--- UliEngineering/Physics/Capacitors.py
+++ UliEngineering/Physics/Capacitors.py
@@ -1,44 +1,28 @@
 #!/usr/bin/env python3
+# -*- coding: utf-8 -*-
 import scipy.constants
-from .Resistors import *
-from .EngineerIO import *
-import math
+from UliEngineering.EngineerIO import normalizeEngineerInputIfStr, formatValue
 import numpy as np
 
-def capacitorEnergy(capacitance, voltage):
+def capacitor_energy(capacitance, voltage):
     """
     Compute the total energy stored in a capacitor given:
     - The capacitance in farads
     - The voltage the capacitor is charged to
     The energy is returned as joules.
-
-    >>> capacitorEnergy("1.5 F", "5.0 V")
-    18.75
-    >>> capacitorEnergy("1.5 F", "0.0 V")
-    0.0
     """
     capacitance, _ = normalizeEngineerInputIfStr(capacitance)
     voltage, _ = normalizeEngineerInputIfStr(voltage)
     return 0.5 * capacitance * np.square(voltage)
 
-def capacitorCharge(capacitance, voltage):
+def capacitor_charge(capacitance, voltage):
     """
     Compute the total charge stored in a capacitor given:
     - The capacitance in farads
     - The voltage the capacitor is charged to
-    The energy is returned in coulombs.
-
-    >>> capacitorCharge("1.5 F", "5.0 V")
-    7.5
-    >>> capacitorCharge("1.5 F", "0.0 V")
-    0.0
+    The charge is returned in coulombs.
     """
     capacitance, _ = normalizeEngineerInputIfStr(capacitance)
     voltage, _ = normalizeEngineerInputIfStr(voltage)
     return capacitance * voltage
 
-if __name__ == "__main__":
-    import doctest
-    doctest.testmod()
-    # Usage example
-    print(formatValue(capacitorEnergy("100 mF", "1.2 V"), "J"))
---
+++ tests/Physics/TestCapacitors.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+from numpy.testing import assert_approx_equal, assert_allclose
+from nose.tools import assert_equal
+from UliEngineering.Physics.Capacitors import *
+import numpy as np
+
+class TestCapacitors(object):
+    def test_capacitor_energy(self):
+        assert_approx_equal(capacitor_energy("1.5 F", "5.0 V"), 18.75)
+        assert_approx_equal(capacitor_energy("1.5 F", "0.0 V"), 0.0)
+        assert_equal(formatValue(capacitor_energy("100 mF", "1.2 V"), "J"), "72.0 mJ")
+
+    def test_capacitor_charge(self):
+        assert_approx_equal(capacitor_charge("1.5 F", "5.0 V"), 7.5)
+        assert_approx_equal(capacitor_charge("1.5 F", "0.0 V"), 0.0)
+
+    def test_numpy_arrays(self):
+        l = np.asarray([1.5, 0.1])
+        assert_allclose(capacitor_energy(l, 5.0), [18.75, 1.25])
