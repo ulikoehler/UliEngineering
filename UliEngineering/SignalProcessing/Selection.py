db03171d245a6babd8168130e4500f63cdbbafc5
Uli Köhler <ulikoehler@online.de>
2016-01-26 18:53:06 +0100

    Added selection module

---
+++ UliEngineering/SignalProcessing/Selection.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Utilities for selecting and finding specific attributes in datasets
+"""
+import numpy as np
+import datetime
+from bisect import bisect_left
+
+__all__ = ["selectByDatetime"]
+
+def selectByDatetime(timestamps, time, factor=1.0, around=None, ofs=0.0):
+    """
+    Find the index in a timestamp array that is closest to a given value.
+    The timestamp array expected to be sorted in ascending order. It is
+    expected to contain Epoch timestamps as either integral or floating-point type.
+
+    This function does perform any copying or modification of the array, so it is suitable
+    for huge mmapped arrays.
+
+    Depending on the scale of the timestamps, the factor argument needs to be adjusted
+    accordingly. For example, 1e3 means that a difference of 1.0 in two timestamps means
+    a difference of 1.0 ms. A factor of 1e6 means that a difference
+    of 1.0 in two timestamp values means a difference of 1.0 µs. The default factor of 1.0
+    means that 1.0 difference in two timestamps means a difference of 1.0 second and therefore
+    is compatible with datetime.timestamp() values
+
+    time may be either a datetime argument or a string in the %Y-%m-%d %H:%M:%S format.
+    If the time string contains a decimal dot, the %Y-%m-%d %H:%M:%S.%f format, which
+    allows specification up to microseconds, is used.
+
+    Optionally, an offset value may be given that is substracted from the
+    calculated timestamp before applying the factor.
+    When using a timestamp offset of 1e9 with a factor of 1.0, for example,
+    the value in the timestamp array for a given date is expected to be 1e9 less
+    than the epoch time.
+
+    This function uses bisect_left internally. Refer to its documentation
+    for exact behavioural description.
+
+    If around is None, returns the idx of the closest match.
+    Else, returns (idx - around, idx + around), i.e. with idx right in the middle.
+    This allows easy generation of ranges centered around a specific value.
+    """
+    if isinstance(time, str):
+        if "." in time:
+            time = datetime.datetime.strptime(time, "%Y-%m-%d %H:%M:%S.%f")
+        else:  # No microsecond part
+            time = datetime.datetime.strptime(time, "%Y-%m-%d %H:%M:%S")
+    idx = bisect_left(timestamps, (time.timestamp() - ofs) * factor)
+    if around is None:
+        return idx
+    else:  # Return range
+        return (idx - around, idx + around)
---
+++ tests/SignalProcessing/TestSelection.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+from numpy.testing import assert_approx_equal, assert_allclose
+from nose.tools import assert_equal, assert_true, raises, assert_less
+from UliEngineering.SignalProcessing.Selection import *
+from nose_parameterized import parameterized
+import concurrent.futures
+import numpy as np
+import datetime
+
+executor = concurrent.futures.ThreadPoolExecutor(4)
+
+class TestSelection(object):
+    def testSelectByDatetime(self):
+        now = datetime.datetime.now()
+        # Test out of range
+        assert_equal(selectByDatetime(np.arange(10), now), 10)
+        # Test in range
+        ts = now.timestamp()
+        seconds50 = datetime.timedelta(seconds=50)
+        r = np.arange(ts, ts + 100)
+        assert_equal(selectByDatetime(r, now + seconds50), 50)
+        # Test with string
+        assert_equal(selectByDatetime(np.arange(10), "2015-01-29 23:59:01"), 10)
+        # Test with string w/ microseconds
+        assert_equal(selectByDatetime(np.arange(10), "2015-01-29 23:59:01.000001"), 10)
+        # Test with around set
+        assert_equal(selectByDatetime(np.arange(10), now, around=5), (5, 15))
+
+    @parameterized.expand([
+        ("whatever"),
+        ("2015-01-2923:59:01"),  # Missing space
+        ("2015-01-29 23:59:01,000001"),  # Comma instead of point
+        ("2015-01-29 23:59:01.00001"),  # Not enough digits
+        ("2015-01-29 23:59:01.0000001"),  # Too many digits
+        ("2015-01-29 23:59:01.00000a1"),  # a is not a digit
+    ])
+    @raises
+    def testInvalidStringFormat(self, str):
+        selectByDatetime(np.arange(10), "2015-01-29 23:59:01")
+
+    @raises
+    def testNoneArray(self, str):
+        selectByDatetime(None, "2015-01-29 23:59:01")
+
+    @raises
+    def testNoneTimestamp(self, str):
+        selectByDatetime(np.arange(10), None)
