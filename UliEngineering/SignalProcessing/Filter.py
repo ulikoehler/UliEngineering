90ff99c38786bd705dfebdbb0b95fea416bcb55e
Uli Köhler <ulikoehler@online.de>
2016-01-28 03:52:07 +0100

    Started to improve filter flexibility, still some tests failing

--- UliEngineering/SignalProcessing/Filter.py
+++ UliEngineering/SignalProcessing/Filter.py
@@ -11,15 +11,21 @@ Features include:
     - Lets you enter the corner frequencies as Hz,
     - Direct frequency response generation
     - Filters implent __call__ for direct filtfilt application
-    - Chainable filter to deal with complex characteristics or unstable high-order filters
+    - ChainFilter to deal with complex characteristics or unstable high-order filters
+    - SumFilter that add the components of multiple individual filters to easily combine multiple
+      bandpass filters
+    - SumFilter and ChainFilter are arbitrarily combinable
+    - Intuitive, readable error messages for non-mathematicians
 """
+from UliEngineering.EngineerIO import autoNormalizeEngineerInputNoUnit
 from scipy import signal
 import numpy as np
 import numbers
+import collections
 from toolz import functoolz
 
 __all__ = ["NotComputedException", "FilterUnstableError", "SignalFilter",
-           "ChainedFilter"]
+           "ChainedFilter", "SumFilter"]
 
 
 class NotComputedException(Exception):
@@ -34,14 +40,42 @@ class FilterUnstableError(Exception):
 
 class SignalFilter(object):
     """
-    High-level abstraction of a digital signal filter
+    High-level abstraction of a digital signal filter.
     """
-    def __init__(self, fs, freqs):
+    def __init__(self, fs, freqs, btype="lowpass"):
+        """
+        Initialize a new filter
+
+        Keyword arguments:
+            fs: The sampling rate
+            freqs: The frequency (for lopass/hipass) or a list of two frequencies
+        """
         self.nyq = fs * 0.5
-        self.fs = fs
+        self.fs = autoNormalizeEngineerInputNoUnit(fs)
         self.b = None
         self.a = None
+        # Normalize freqs: Allow [1.0] instead of 1.0
+        if isinstance(freqs, collections.Iterable) and not isinstance(freqs, str):
+            if len(freqs) == 1:
+                freqs = freqs[0]
+            elif len(freqs) == 0:
+                raise ValueError("Empty frequency list")
+            elif isinstance(freqs[0], str):
+                freqs = [autoNormalizeEngineerInputNoUnit(f) for f in freqs]
+        # Allow "4.5 kHz" etc
+        if isinstance(freqs, str):
+            freqs = autoNormalizeEngineerInputNoUnit
         self.freqs = self._filtfreq(freqs)
+        # Check & store pass type
+        if btype == "lowpass" or btype == "highpass":
+            if not isinstance(freqs, numbers.Number):
+                raise ValueError("Pass-type {0} requires a single critical frequency, not {1}".format(btype, freqs))
+        elif btype == "bandpass" or btype == "bandstop":
+            if isinstance(freqs, numbers.Number) or len(freqs) != 2:
+                raise ValueError("Pass-type {0} requires a two critical frequencies, not {1}".format(btype, freqs))
+        else:
+            raise ValueError("Invalid pass type '{0}': Use lowpass, highpass, bandpass or bandstop!".format(btype))
+        self.btype = btype
 
     def _filtfreq(self, f):
         """
@@ -62,13 +96,13 @@ class SignalFilter(object):
             raise NotComputedException()
         return not np.any(np.abs(np.roots(self.a)) > 1.0)
 
-    def iir(self, order, btype="bandpass", ftype="butter", rp=0.01, rs=100.0):
+    def iir(self, order, ftype="butter", rp=0.01, rs=100.0):
         """
         Generate filter coefficients for an arbitrary IIR filter
 
         Returns the current instance so it can be chained inline
         """
-        self.b, self.a = signal.iirfilter(order, self.freqs, btype=btype,
+        self.b, self.a = signal.iirfilter(order, self.freqs, btype=self.btype,
                                           ftype=ftype, rp=rp, rs=rs)
         if not self.is_stable():
             self.a = self.b = None
@@ -88,6 +122,7 @@ class SignalFilter(object):
             raise NotComputedException()
         return signal.filtfilt(self.b, self.a, d)
 
+
 class ChainedFilter(object):
     """
     Chained filter object that applies a number of filters in series.
@@ -96,18 +131,56 @@ class ChainedFilter(object):
     filtfilt is used to avoid phase issues by repeated application.
 
     Frequency response plotting is currently unsupported.
+
+    Filters can be added to the end of the chain via +=
     """
     def __init__(self, filters, repeat=1):
         "The first filter in the filters list is applied first"
         if isinstance(filters, SignalFilter):
             filters = [filters]
         self.filters = filters
-        self.unique_filters = filters  # Will not contain repeat duplicates
         if repeat > 1:
             self.filters *= repeat
+
+    def __iadd__(self, f):
+        "Add a filter to the end of the chain"
+        self.filters.append(f)
+        return self
+
+    def __len__(self):
+        return len(self.filters)
 
     def __call__(self, d):
         return functoolz.pipe(d, *self.filters)
 
     def is_stable(self):
-        return all([f.is_stable() for f in self.filters])
+        # Performance not considered important here. User will usually call this once
+        return all(f.is_stable() for f in self.filters)
+
+
+class SumFilter(object):
+    """
+    Chained filter object that applies a number of filters and sums the results.
+    This can be used to combine multiple bandpass filters for multiple passbands.
+
+    Filters can be added via +=
+    """
+    def __init__(self, filters):
+        "The first filter in the filters list is applied first"
+        if isinstance(filters, SignalFilter):
+            filters = [filters]
+        self.filters = filters
+
+    def __iadd__(self, f):
+        "Add a filter to the end of the chain"
+        self.filters.append(f)
+        return self
+
+    def __len__(self):
+        return len(self.filters)
+
+    def __call__(self, d):
+        return sum(filt(d) for filt in self.filters)
+
+    def is_stable(self):
+        return all(f.is_stable() for f in self.filters)
--- tests/SignalProcessing/TestFilter.py
+++ tests/SignalProcessing/TestFilter.py
@@ -17,28 +17,10 @@ class TestFilter(object):
         ("bandstop", [1.0, 2.0]),
     ])
     def testBasicFilter(self, btype, frequencies):
-        filt = SignalFilter(100.0, frequencies)
-        filt.iir(order=3, btype=btype)
+        filt = SignalFilter(100.0, frequencies, btype=btype)
+        filt.iir(order=3)
         d2 = filt(self.d)
         assert_equal(self.d.shape, d2.shape)
-
-
-    @parameterized([
-        ("lowpass", 1.0),
-        ("highpass", 1.0),
-        ("bandpass", [1.0, 2.0]),
-        ("bandstop", [1.0, 2.0]),
-    ])
-    def testChainedFilter(self, btype, frequencies):
-        filt = SignalFilter(100.0, frequencies).iir(order=3, btype=btype)
-        cfilt = ChainedFilter([filt, filt])
-        d2 = cfilt(self.d)
-        assert_equal(self.d.shape, d2.shape)
-        # Check repeat
-        cfilt = ChainedFilter(filt, repeat=4)
-        d2 = cfilt(self.d)
-        assert_equal(self.d.shape, d2.shape)
-        assert_true(cfilt.is_stable())
 
     @parameterized([
         ("butter",),
@@ -48,16 +30,52 @@ class TestFilter(object):
         ("bessel",),
     ])
     def testFilterTypes(self, ftype):
-        filt = SignalFilter(100.0, [1.0, 2.0])
+        filt = SignalFilter(100.0, [1.0, 2.0], btype="bandpass")
         filt.iir(order=3, ftype=ftype)
         d2 = filt(self.d)
         assert_equal(self.d.shape, d2.shape)
 
     def testFrequencyResponse(self):
-        filt = SignalFilter(100.0, [1.0, 2.0])
+        filt = SignalFilter(100.0, [1.0, 2.0], btype="bandpass")
         filt.iir(order=3)
         filt.frequency_response()
 
+    @parameterized([
+        ("lowpass", 1.0),
+        ("highpass", 1.0),
+        ("bandpass", [1.0, 2.0]),
+        ("bandstop", [1.0, 2.0]),
+        ("lowpass", [1.0]),
+        ("highpass", [1.0]),
+        ("lowpass", "1.0"),
+        ("highpass", "1.0"),
+        ("lowpass", ["1.0"]),
+        ("highpass", ["1.0"]),
+        ("bandpass", ["1.0", "2.0"]),
+        ("bandstop", ["1.0", "2.0"]),
+        ("lowpass", "1.0 kHz"),
+        ("lowpass", "47.532 µHz"),
+    ])
+    def testCorrectFrequencyParam(self, btype, freqs):
+        SignalFilter(100.0, freqs, btype)
+
+    @parameterized([
+        ("lowpass", [1.0, 2.0]),
+        ("highpass", [1.0, 2.0]),
+        ("bandpass", 1.0),
+        ("bandstop", 1.0),
+        ("lowpass", [1.0, 2.0, 3.0]),
+        ("highpass", [1.0, 2.0, 3.0]),
+        ("bandpass", [1.0, 2.0, 3.0]),
+        ("bandstop", [1.0, 2.0, 3.0]),
+        ("lowpass", "foobar"),
+        ("highpass", "foobar"),
+        ("bandpass", "foobar"),
+        ("bandstop", "foobar"),
+    ])
+    @raises(ValueError)
+    def testWrongFrequencyParam(self, btype, freqs):
+        SignalFilter(100.0, freqs, btype)
 
     @raises(NotComputedException)
     def testUninitializedFilter1(self):
@@ -71,5 +89,50 @@ class TestFilter(object):
 
     @raises(FilterUnstableError)
     def testUnstableFilter(self):
-        filt = SignalFilter(100.0, [1.0, 2.0])
+        filt = SignalFilter(100.0, [1.0, 2.0], btype="bandpass")
         filt.iir(order=100, rp=1e-12)
+
+class TestChainedFilter(TestFilter):
+
+    @parameterized([
+        ("lowpass", 1.0),
+        ("highpass", 1.0),
+        ("bandpass", [1.0, 2.0]),
+        ("bandstop", [1.0, 2.0]),
+    ])
+    def testBasic(self, btype, frequencies):
+        filt = SignalFilter(100.0, 1.0, btype="lowpass").iir(order=3)
+        cfilt = ChainedFilter([filt, filt])
+        d2 = cfilt(self.d)
+        assert_equal(self.d.shape, d2.shape)
+        # Check repeat
+        cfilt = ChainedFilter(filt, repeat=4)
+        d2 = cfilt(self.d)
+        assert_equal(self.d.shape, d2.shape)
+        assert_true(cfilt.is_stable())
+        # Check +=
+        len1 = len(cfilt)
+        cfilt += filt
+        d2 = cfilt(self.d)
+        assert_equal(len(cfilt), len1 + 1)
+        assert_equal(self.d.shape, d2.shape)
+
+
+class TestSumFilter(TestFilter):
+    @parameterized([
+        ("lowpass", 1.0),
+        ("highpass", 1.0),
+        ("bandpass", [1.0, 2.0]),
+        ("bandstop", [1.0, 2.0]),
+    ])
+    def testBasic(self, btype, frequencies):
+        filt = SignalFilter(100.0, 1.0, btype="lowpass").iir(order=3)
+        sfilt = SumFilter([filt, filt])
+        d2 = sfilt(self.d)
+        assert_equal(self.d.shape, d2.shape)
+        # Check +=
+        len1 = len(sfilt)
+        sfilt += filt
+        d2 = sfilt(self.d)
+        assert_equal(len(sfilt), len1 + 1)
+        assert_equal(self.d.shape, d2.shape)
