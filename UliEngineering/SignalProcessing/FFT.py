Fix FFT normalization & add unit test for FFT integral

UliEngineering/SignalProcessing/FFT.py
@@ -32,7 +32,7 @@ def computeFFT(y, samplerate, window="blackman"):
     n = len(y)
     windowArr = __fft_windows[window](n)
     w = scipy.fftpack.fft(y * windowArr)
-    w = 2.0 * np.abs(w[:n / 2]) / (samplerate * n) # Perform amplitude normalization
+    w = 2.0 * np.abs(w[:n / 2]) / n  # Perform amplitude normalization
     x = np.linspace(0.0, samplerate / 2, n / 2)
     return (x, w)
 
@@ -85,7 +85,7 @@ def parallelFFTReduce(chunkgen, samplerate, fftsize, removeDC=False, window="bla
     x = np.linspace(0.0, samplerate / 2, fftsize / 2)
     fftSum = reducer((f.result() for f in concurrent.futures.as_completed(futures)))
     # Perform normalization once
-    return (x, 2.0 * (fftSum / (len(chunkgen) * samplerate * fftsize))) if normalize else fftSum
+    return (x, 2.0 * (fftSum / (len(chunkgen) * fftsize))) if normalize else fftSum
 
 
 def simpleParallelFFTReduce(arr, samplerate, fftsize, shiftsize=None, nthreads=4, chunkfunc=None, **kwargs):

tests/SignalProcessing/TestFFT.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 from numpy.testing import assert_approx_equal, assert_allclose
-from nose.tools import assert_equal, assert_true, raises, assert_less
+from nose.tools import assert_equal, assert_true, raises, assert_less, assert_almost_equal
 from UliEngineering.SignalProcessing.FFT import *
 from UliEngineering.SignalProcessing.Chunks import *
 from nose_parameterized import parameterized
@@ -72,6 +72,19 @@ class TestFFT(object):
         assert_equal(dominantFrequency((x, y)), 132)
         # Check with frequency range
         assert_equal(dominantFrequency(x, y, low=100.0, high=140.0), 132)
+
+    @parameterized.expand([
+        (1.,),
+        (10.234,),
+        (0.01,),
+        (10000.,),
+    ])
+    def testFFTAmplitudeIntegral(self, amplitude):
+        """FFT amplitude integral should be equal to ptp value of a sine wave"""
+        sine = generate_sinewave(10.0, 100.0, amplitude, 2000)
+        fftx, ffty = computeFFT(sine, 100.0)
+        # Number of decimals must depend on value, so we need to divide here
+        assert_almost_equal(np.sum(ffty) / amplitude, 1.0, 3)
 
     @raises(ValueError)
     def test_fft_empty_chunks(self):