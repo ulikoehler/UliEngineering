fde56c27762bb96a463bf19a0780fe1e96b98cfd
Uli Köhler <ulikoehler@online.de>
2016-02-02 21:30:42 +0100

    More coverage

--- tests/SignalProcessing/TestResampling.py
+++ tests/SignalProcessing/TestResampling.py
@@ -4,32 +4,38 @@ from nose.tools import assert_equal, assert_true, raises
 from numpy.testing import assert_allclose
 from UliEngineering.SignalProcessing.Resampling import *
 
+
 class TestResampling(object):
+    def __init__(self):
+        self.x = np.arange(100)
+        self.y = np.square(self.x)
+
     def testDiscard(self):
         x = np.arange(10)
         assert_allclose(resample_discard(x, 2), [0, 2, 4, 6, 8])
         assert_allclose(resample_discard(x, 3), [0, 3, 6, 9])
 
     def testBSplineResampler(self):
-        x = np.arange(100)
-        y = np.square(x)
-        y2 = BSplineResampler(x, y, time_factor=1).resample_to(1)
+        y2 = BSplineResampler(self.x, self.y, time_factor=1).resample_to(1)
 
     def testResampledFilteredXYView(self):
-        x = np.arange(100)
-        y = np.square(x)
         # TODO improve test
-        y2 = ResampledFilteredXYView(x, y, 1.0, 1.0)[30:80]
+        y2 = ResampledFilteredXYView(self.x, self.y, 1.0, 1.0)[30:80]
 
     def testResampledFilteredView(self):
-        x = np.arange(100)
-        y = np.square(x)
         # TODO improve test
-        y2 = ResampledFilteredView(x, y, 1.0, 1.0)[30:50]
+        y2 = ResampledFilteredView(self.x, self.y, 1.0, 1.0)[30:50]
 
     def testResampledFilteredView(self):
-        x = np.arange(100)
-        y = np.square(x)
         # TODO improve test
-        y2 = ResampledFilteredView(x, y, 1.0, 1.0)[30:50]
-        
+        y2 = ResampledFilteredView(self.x, self.y, 1.0, 1.0)[30:50]
+
+    @raises(TypeError)
+    def testResampledFilteredXYViewInvalidArgs(self):
+        rv = ResampledFilteredXYView(self.x, self.y, 100.)
+        rv[1]
+
+    @raises(TypeError)
+    def testResampledFilteredXYViewInvalidArgs2(self):
+        rv = ResampledFilteredXYView(self.x, self.y, 100.)
+        rv[self]
