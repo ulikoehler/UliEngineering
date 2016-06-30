e7c252f4317dab52822770846ca0a02d3221b36a
Uli Köhler <ulikoehler@online.de>
2016-07-01 01:30:55 +0200

    Implemented string splitting function that only takes the nth token

---
+++ UliEngineering/Utils/String.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+"""
+String utilities and algorithms
+"""
+
+def split_nth(s, delimiter=",", nth=1):
+	"""
+	Like s.split(delimiter), but only returns the nth string of split's return array.
+	Other strings or the split list itself are not generated, therefore using this function
+	might be more efficient in many usecases where only
+	Does not support regex delimiters.
+
+	Throws ValueError if the nth delimiter has not been found
+	"""
+	# Find start index
+	if nth <= 0:
+		raise ValueError("Invalid nth parameter: Must be >= 0 but value is {0}".format(nth))
+	if nth == 1:
+		startidx = 0
+	else:
+		nth_tmp = nth # This one is counted down
+		for startidx in range(len(s)):
+			if s[startidx] == delimiter:
+				nth_tmp -= 1
+				if nth_tmp <= 1:
+					break
+		# Possibly we did not find enough delimiters
+		if startidx == len(s) - 1 and s[-1] != delimiter:
+			raise ValueError("nth value out of range: Not enough '{0}'' delimiters in '{1}'".format(delimiter, s))
+	# Find end index
+	endidx = None
+	for endidx in range(startidx + 1, len(s)):
+		if s[endidx] == delimiter:
+			break
+	# If we did not find the end delimiter, we must avoid skipping the last character,
+	# But a delimiter must not be assumed as last character
+	if endidx == len(s) - 1 and s[-1] != delimiter:
+		endidx = None
+	return s[startidx + 1 if nth > 1 else 0:endidx]
---
+++ tests/Utils/TestString.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+from nose.tools import assert_equal, assert_true, assert_false, raises
+from UliEngineering.Utils.String import *
+from nose_parameterized import parameterized
+
+class TestSplitNth(object):
+    def testSimple(self):
+        assert_equal("a", split_nth("a,b,c,d,e,f"))
+        assert_equal("a", split_nth("a,b,c,d,e,f", nth=1))
+        assert_equal("b", split_nth("a,b,c,d,e,f", nth=2))
+        assert_equal("c", split_nth("a,b,c,d,e,f", nth=3))
+        assert_equal("d", split_nth("a,b,c,d,e,f", nth=4))
+        assert_equal("e", split_nth("a,b,c,d,e,f", nth=5))
+        assert_equal("f", split_nth("a,b,c,d,e,f", nth=6))
+
+    def testMultiChar(self):
+        assert_equal("ab", split_nth("ab,cd,ef,gh,ij,kl"))
+        assert_equal("ab", split_nth("ab,cd,ef,gh,ij,kl", nth=1))
+        assert_equal("cd", split_nth("ab,cd,ef,gh,ij,kl", nth=2))
+        assert_equal("ef", split_nth("ab,cd,ef,gh,ij,kl", nth=3))
+        assert_equal("gh", split_nth("ab,cd,ef,gh,ij,kl", nth=4))
+        assert_equal("ij", split_nth("ab,cd,ef,gh,ij,kl", nth=5))
+        assert_equal("kl", split_nth("ab,cd,ef,gh,ij,kl", nth=6))
+
+    def testOtherDelimiter(self):
+        assert_equal("a", split_nth("a;b;c;d;e;f", delimiter=';'))
+        assert_equal("a", split_nth("a;b;c;d;e;f", delimiter=';', nth=1))
+        assert_equal("b", split_nth("a;b;c;d;e;f", delimiter=';', nth=2))
+        assert_equal("c", split_nth("a;b;c;d;e;f", delimiter=';', nth=3))
+        assert_equal("d", split_nth("a;b;c;d;e;f", delimiter=';', nth=4))
+        assert_equal("e", split_nth("a;b;c;d;e;f", delimiter=';', nth=5))
+        assert_equal("f", split_nth("a;b;c;d;e;f", delimiter=';', nth=6))
+        assert_equal("", split_nth("aa;bb;;dd;ee;ff", delimiter=';', nth=3))
+        assert_equal("", split_nth("aa;bb;;", delimiter=';', nth=3))
+        assert_equal("", split_nth("aa;bb;;", delimiter=';', nth=4))
+
+    def testEmpty(self):
+        assert_equal("", split_nth("aa,bb,,dd,ee,ff", nth=3))
+        assert_equal("", split_nth("aa,bb,,", nth=3))
+        assert_equal("", split_nth("aa,bb,,", nth=4))
+        assert_equal("", split_nth("", nth=1))
+
+    @parameterized(["a", "abc", "abcdef", "abc,def"])
+    @raises(ValueError)
+    def testInvalidFirst(self, param):
+        split_nth(param, nth=3)
+
+    @raises(ValueError)
+    def testInvalidOther(self):
+        split_nth("abc,def", nth=3)
+
