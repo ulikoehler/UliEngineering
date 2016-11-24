9fc92a176fbc9425229d02a032d3494566139e6a
Uli Köhler <ulikoehler@online.de>
2016-11-24 17:15:26 +0100

    Add more NTC unit tests

--- tests/Physics/TestNTC.py
+++ tests/Physics/TestNTC.py
@@ -12,5 +12,6 @@ class TestNTC(object):
         # Values arbitrarily from Murata NCP15WB473D03RC
         assert_approx_equal(ntc_resistance("47k", "4050K", "25°C"), 47000)
         assert_approx_equal(ntc_resistance("47k", "4050K", "0°C"), 162942.79)
+        assert_approx_equal(ntc_resistance("47k", "4050K", "-18°C"), 463773.791)
         assert_approx_equal(ntc_resistance("47k", "4050K", "5°C"), 124819.66)
         assert_approx_equal(ntc_resistance("47k", "4050K", "60°C"), 11280.407)
