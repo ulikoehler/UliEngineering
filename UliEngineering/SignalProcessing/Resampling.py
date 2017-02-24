abb1dcaee043b5872e3fd44b92324e6dbcb6e3ce
Uli Köhler <ulikoehler@online.de>
2017-02-24 03:28:53 +0100

    Remove old resampling classes which never really worked and were never used. Will be replaced by better code some time in the future.

--- UliEngineering/SignalProcessing/Resampling.py
+++ UliEngineering/SignalProcessing/Resampling.py
@@ -13,159 +13,60 @@ from toolz.functoolz import identity
 from UliEngineering.Utils.Concurrency import new_thread_executor
 from .Utils import LinRange
 
-__all__ = ["resample_discard", "BSplineResampler", "ResampledFilteredXYView",
-           "ResampledFilteredView", "ResampledFilteredViewYOnlyDecorator",
+__all__ = ["resample_discard", "resampled_timespace",
            "parallel_resample"]
-
-class BSplineResampler(object):
-    """
-    Arbitrary resampler using scipy bsplines
-    """
-    def __init__(self, fx, fy, time_factor=1e6, prefilt=None):
-        if prefilt is not None:
-            fy = prefilt(fy)
-        self.fx = fx
-        self.fy = fy
-        self.time_factor = time_factor
-        # Generate bspline
-        self.tck = splrep(fx, fy)
-
-    @property
-    def samplerate_stddev(self):
-        "Get the variance of the differential source sample rate"
-        return np.stddev(np.reciprocate(np.diff(self.fx)))
-
-    @property
-    def actual_samplerate(self):
-        return self.time_factor / np.mean(np.diff())
-
-    def resample(self, samples):
-        "Resample with a specific sample array. Alle samples must lie within the original samplespace"
-        y = splev(samples, self.tck, ext=2) # ext: raise ValueError on out of bounds access
-        return samples, y
-
-    def resample_to(self, samplerate):
-        "Resample to a specific samplerate. Returns fx, fy"
-        num_samples = (self.fx[-1] - self.fx[0]) * samplerate / self.time_factor
-        return self.resample(np.linspace(self.fx[0], self.fx[-1], num_samples))
 
 
 def resample_discard(arr, divisor, ofs=0):
     """
     Resample with an integral divisor, discarding all other samples.
     Returns a view of the data.
+    Very fast as this doesn't need to read the data.
     """
     return arr[ofs::divisor]
 
-
-class ResampledFilteredXYView(object):
+def resampled_timespace(t, new_samplerate, assume_sorted=True, time_factor=1e6):
     """
-    Lazy resampling 1D array view that can be used e.g. as input for a chunk generator.
-    Note that this class will generate an ill-sized last slice in most cases.
-    The user must therefore ensure that downstream functions appropriately deal with those slices.
-    One way of doing that is to oversize the slices requested by the caller and ensure they are properly
-    trimmed downstream.
-
-    This class returns (xslice, yslice) on slice. See ResampledFilteredView for a wrapper that only
-    returns the Y view.
-    """
-    def __init__(self, fx, fy, target_samplerate, time_factor=1e6, filt=None):
-        self.fx = fx
-        self.fy = fy
-        self.samplerate = target_samplerate
-        self.filt = filt
-        self.time_factor = time_factor
-
-    def __getitem__(self, key):
-        if isinstance(key, slice):
-            xslice = self.fx.__getitem__(key)
-            yslice = self.fy.__getitem__(key)
-            resampler = BSplineResampler(xslice, yslice, time_factor=self.time_factor,
-                                         prefilt=self.filt)
-            return resampler.resample_to(self.samplerate)
-        elif isinstance(key, int):
-            raise TypeError("ResampledFilteredXYView can only be sliced with slice indices, not single numbers")
-        else:
-            raise TypeError("Invalid argument type for slicing: {0}".format(type(key))) 
-
-    @property
-    def shape(self):
-        return self.fy.shape
-
-class _XViewDecorator(object):
-    "This class is used in ResampledFilteredView to provide the time object"
-    def __init__(self, delegate):
-        self.delegate = delegate
-
-    def __getitem__(self, key):
-        if isinstance(key, slice):
-            x, _ = ResampledFilteredXYView.__getitem__(self.delegate, key)
-            return x
-        elif isinstance(key, int):
-            raise TypeError("ResampledFilteredView.time can only be sliced with slice indices, not single numbers")
-        else:
-            raise TypeError("Invalid argument type for slicing: {0}".format(type(key)))
-
-class ResampledFilteredView(ResampledFilteredXYView):
-    "Like ResampledFilteredXYView, but only returns y values on slice"
-
-    @property
-    def time(self):
-        return _XViewDecorator(self)
-
-    def __getitem__(self, key):
-        if isinstance(key, slice):
-            _, y = super().__getitem__(key)
-            return y
-        elif isinstance(key, int):
-            raise TypeError("ResampledFilteredView can only be sliced with slice indices, not single numbers")
-        else:
-            raise TypeError("Invalid argument type for slicing: {0}".format(type(key))) 
+    Compute the new timespace after resampling a source timestamp array
+    (not neccessarily lazy)
 
 
-class ResampledFilteredViewYOnlyDecorator(object):
-    """
-    Like ResampledFilteredXYView, but only returns y values on slice.
-    Decorator version of ResampledFilteredView.
-
-    Usage example:
-        rv = ResampledFilteredXYView(...)
-        rv_yonly = ResampledFilteredViewYOnlyDecorator(rv)
-    """
-    def __init__(self, other):
-        self.other = other
-
-    def __getitem__(self, key):
-        if isinstance(key, slice):
-            _, y = self.other.__getitem__(key)
-            return y
-        elif isinstance(key, int):
-            raise TypeError("ResampledFilteredView can only be sliced with slice indices, not single numbers")
-        else:
-            raise TypeError("Invalid argument type for slicing: {0}".format(type(key)))
-
-    @property
-    def shape(self):
-        return self.other.shape
-
-
-def __compute_new_samples(t, new_samplerate, assume_sorted=True, time_factor=1e6):
-    """
-    Compute a lazy LinRange of new sample times when resampling
-    a given time array with a new samplerate.
-    Also checks if the given time array is large enough for proper resampling.
+    Parameters
+    ----------
+    t : numpy array-like
+        The source timestamps
+    new_samplerate : float
+        The new datarate in Hz
+    assume_sorted : bool
+        If this is True, the code assumes the source
+        timestamp array is monotonically increasing, i.e.
+        the lowest timestamp comes first and the highest last.
+        If this is False, the code determines
+        the min/max value by reading the entire array.
+    time_factor : float
+        Defines what timestamps in the source (and result)
+        array means. This is required to interpret new_samplerate.
+        If time_factor=1e6, it means that a difference of 1.0
+        in two timestamps means a difference of 1/1e6 seconds.
+    
+    Returns
+    -------
+    A LinSpace() (acts like a numpy array but doesn't consume any memory)
+    that represents the new timespace
+    news
     """
     if len(t) == 0:
         raise ValueError("Empty time array given - can not perform any resampling")
     if len(t) == 1:
         raise ValueError("Time array has only one value - can not perform any resampling")
-    # Compute time corners
-    sample_tdelta = time_factor / new_samplerate
+    # Compute time endpoints
+    dst_tdelta = time_factor / new_samplerate
     startt, endt = (t[0], t[-1]) if assume_sorted else (np.min(t), np.max(t))
-    tdelta = endt - startt
-    if tdelta < sample_tdelta:
+    src_tdelta = endt - startt
+    if src_tdelta < dst_tdelta:
         raise ValueError("The time delta is smaller than a single sample - can not perform resampling")
-    return LinRange.range(startt, endt, sample_tdelta)
+    # Use a lazy linrange to represent time interval
+    return LinRange.range(startt, endt, dst_tdelta)
 
 
 def __parallel_resample_worker(torig, tnew, y, out, i, degree, chunksize, ovp_size, prefilter):
@@ -225,7 +126,7 @@ def parallel_resample(t, y, new_samplerate, out=None, prefilter=None,
 
     Return the output array.
     """
-    new_t = __compute_new_samples(t, new_samplerate, time_factor=time_factor)
+    new_t = resampled_timespace(t, new_samplerate, time_factor=time_factor)
     # Lazily compute the new timespan
     if out is None:
         out = np.zeros(len(new_t))
--- tests/SignalProcessing/TestResampling.py
+++ tests/SignalProcessing/TestResampling.py
@@ -15,35 +15,6 @@ class TestBSplineResampling(object):
         assert_allclose(resample_discard(x, 2), [0, 2, 4, 6, 8])
         assert_allclose(resample_discard(x, 3), [0, 3, 6, 9])
 
-    def testBSplineResampler(self):
-        y2 = BSplineResampler(self.x, self.y, time_factor=1).resample_to(1)
-
-    def testResampledFilteredXYView(self):
-        # TODO improve test
-        y2 = ResampledFilteredXYView(self.x, self.y, 1.0, 1.0)
-        y2[30:80]
-        assert_equal(y2.shape, self.y.shape)
-        assert_raises(TypeError, lambda: y2[1])
-        assert_raises(TypeError, lambda: y2[self])
-
-    def testResampledFilteredView(self):
-        # TODO improve test
-        y2 = ResampledFilteredView(self.x, self.y, 1.0, 1.0)
-        y2[30:50]
-        assert_equal(y2.shape, self.y.shape)
-        assert_raises(TypeError, lambda: y2[1])
-        assert_raises(TypeError, lambda: y2[self])
-
-    def testResampledFilteredViewYOnlyDecorator(self):
-        # TODO improve test
-        y2 = ResampledFilteredXYView(self.x, self.y, 1.0, 1.0)
-        y3 = ResampledFilteredViewYOnlyDecorator(y2)
-        y3[30:80]
-        assert_equal(y3.shape, self.y.shape)
-        assert_raises(TypeError, lambda: y3[1])
-        assert_raises(TypeError, lambda: y3[self])
-
-
 class TestParallelResampling(object):
     def __init__(self):
         self.x = np.arange(100)
