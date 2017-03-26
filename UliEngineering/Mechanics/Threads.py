5fbc7ce0a93ad7360d148a57cacff9a69ed888c4
Uli Köhler <ulikoehler@online.de>
2017-03-26 22:52:18 +0200

    Added metric thread table

---
+++ UliEngineering/Mechanics/Threads.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+"""
+Thread information
+"""
+from collections import namedtuple
+
+ThreadParameters = namedtuple("ThreadParameters",
+    ["pitch", # Thread pitch in mm
+     "outer_diameter", # Outer diameter in mm (for outside thread)
+     "core_diameter" # Core diameter (for outside thread)
+    ])
+
+threads = {
+    # DIN 13
+    # Source: http://www.gewinde-norm.de/metrisches-iso-gewinde-din-13.htm
+    "M1": ThreadParameters(.25, 1., .693),
+    "M1.2": ThreadParameters(.25, 1.2, .893),
+    "M1.6": ThreadParameters(.35, 1.6, 1.171),
+    "M2": ThreadParameters(.40, 2., 1.509),
+    "M2.5": ThreadParameters(.45, 2.5, 1.948),
+    "M3": ThreadParameters(.5, 3., 2.387),
+    "M4": ThreadParameters(.7, 4., 3.141),
+    "M5": ThreadParameters(.8, 5., 4.019),
+    "M6": ThreadParameters(1., 6., 4.773),
+    "M8": ThreadParameters(1.25, 8., 6.466),
+    "M10": ThreadParameters(1.5, 10., 8.160),
+    "M12": ThreadParameters(1.75, 12., 9.853),
+    "M16": ThreadParameters(2., 16., 13.546),
+    "M20": ThreadParameters(2.5, 20., 16.933),
+    "M24": ThreadParameters(3., 24., 20.319),
+    "M36": ThreadParameters(4., 36., 31.093),
+    "M42": ThreadParameters(4.5, 42., 36.479),
+    "M48": ThreadParameters(5., 48., 41.866),
+    "M56": ThreadParameters(5.5, 56., 49.252),
+    "M64": ThreadParameters(6., 64., 56.639)
+]
---
+++ tests/Mechanics/Threads.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+from numpy.testing import assert_approx_equal, assert_allclose
+from nose.tools import assert_equal
+from UliEngineering.Mechanics.Threads import *
+import numpy as np
+
+class TestThreads(object):
+    pass
