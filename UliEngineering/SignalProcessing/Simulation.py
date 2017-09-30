b0956169f9c6b59b267ad7a0e67263dd15896be5
Uli Köhler <ulikoehler@online.de>
2017-09-30 21:37:14 +0200

    Fix generate_sinewave not computing phase shift correctly (now also covered in unit test)

--- UliEngineering/SignalProcessing/Simulation.py
+++ UliEngineering/SignalProcessing/Simulation.py
@@ -3,14 +3,7 @@
 """
 Utilities for FFT computation and visualization
 """
-import scipy.fftpack
 import numpy as np
-import functools
-from toolz import functoolz
-from .Selection import fft_select_frequency_range, find_closest_index
-from .Chunks import overlapping_chunks
-import concurrent.futures
-from UliEngineering.Utils.Concurrency import *
 
 __all__ = ["generate_sinewave"]#, "generate_squarewave"]
 
@@ -26,5 +19,5 @@ def generate_sinewave(frequency, samplerate, amplitude, length, phaseshift=0):
     :param phaseshift The phaseshift in degrees
     """
     x = np.arange(length * samplerate)
-    phaseshift_add = phaseshift * 8 * np.pi * frequency / 360.
+    phaseshift_add = phaseshift * samplerate / (360. * frequency)
     return amplitude * np.sin(frequency * (2. * np.pi) * (x + phaseshift_add) / samplerate)
--- tests/SignalProcessing/TestSimulation.py
+++ tests/SignalProcessing/TestSimulation.py
@@ -15,3 +15,33 @@ class TestGenerateSinewave(object):
         fftx, ffty = compute_fft(sw, 400.)
         df = dominant_frequency(fftx, ffty)
         assert_true(abs(df - 25.0) < 0.1)
+
+
+    @parameterized.expand([
+        (1.,),
+        (2.,),
+        (3.,),
+        (4.,),
+        (5.,),
+        (6.,),
+        (7.,),
+        (8.,),
+        (9.,),
+        (10.,),
+        (10.234,),
+        (11.,),
+    ])
+    def testPhaseShift(self, frequency):
+        """Test if 0/360/720° phase shift matches, and 180/540° matches as wel"""
+        sw0 = generate_sinewave(frequency, 1000.0, amplitude=1., length=5.0, phaseshift=0.0)
+        sw180 = generate_sinewave(frequency, 1000.0, amplitude=1., length=5.0, phaseshift=180.0)
+        sw360 = generate_sinewave(frequency, 1000.0, amplitude=1., length=5.0, phaseshift=360.0)
+        sw540 = generate_sinewave(frequency, 1000.0, amplitude=1., length=5.0, phaseshift=540.0)
+        sw720 = generate_sinewave(frequency, 1000.0, amplitude=1., length=5.0, phaseshift=720.0)
+        # Test in-phase
+        assert_allclose(sw0, sw360, atol=1e-7)
+        assert_allclose(sw0, sw720, atol=1e-7)
+        assert_allclose(sw180, sw540, atol=1e-7)
+        # Test out-of-phase
+        assert_allclose(sw0, -sw180, atol=1e-7)
+
