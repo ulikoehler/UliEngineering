f371bf084233a681727aca42264714c0f758a2f5
Uli Köhler <ulikoehler@online.de>
2019-02-03 06:31:57 +0100

    Added actual_load_capacitance

--- UliEngineering/Electronics/Crystal.py
+++ UliEngineering/Electronics/Crystal.py
@@ -6,9 +6,9 @@ Crystal oscillator utilities
 from UliEngineering.EngineerIO import normalize_numeric
 from UliEngineering.Units import Unit
 
-__all__ = ["load_capacitors"]
+__all__ = ["load_capacitors", "actual_load_capacitance"]
 
-def load_capacitors(cload, cpin="3pF", cstray="2 pF") -> Unit("F"):
+def load_capacitors(cload, cpin="3 pF", cstray="2 pF") -> Unit("F"):
     """
     Compute the load capacitors which should be used for a given crystal,
     given that the load capacitors should be symmetric (i.e. have the same value).
@@ -38,3 +38,39 @@ def load_capacitors(cload, cpin="3pF", cstray="2 pF") -> Unit("F"):
     # => solve A = (B*B) / (B+B) + C for B
     # => solve A = ((B+P)*(B+P)) / ((B+P)+(B+P)) + C for B
     return (2 * (cload - cstray)) - cpin
+
+def actual_load_capacitance(cext, cpin="3 pF", cstray="2 pF") -> Unit("F"):
+    """
+    Compute the actual load capacitance of a crystal given:
+
+    - The external capacitance value (use "10 pF" if your have a
+    10 pF capacitor on each of the crystal pins)
+    - The parasitic pin capacitance
+
+    The value returned should match the load capacitance value
+    in the crystal datasheet.
+
+    Based on (C1 * C2) / (C1 + C2) + Cstray
+
+    If yu use a
+
+    >>> auto_format(actual_load_capacitance, "5 pF", cpin="3 pF", cstray="2pF")
+    '6.00 pF'
+
+    Parameters
+    ----------
+    cext : float
+        The load capacitor value
+    cstray : float
+        The stray capacitance
+    cpin : float
+        The capacitance of one of the oscillator pins of the connected device
+    """
+    cext = normalize_numeric(cext)
+    cstray = normalize_numeric(cstray)
+    cpin = normalize_numeric(cpin)
+    # cload = (C1 * C2) / (C1 + C2) + Cstray where C1 == C2
+    # => solve A = (B*B) / (B+B) + C for B
+    # => solve A = ((B+P)*(B+P)) / ((B+P)+(B+P)) + C for B
+    ctotal = cext + cpin
+    return cstray + ((ctotal * ctotal) / (ctotal + ctotal))
--- tests/Electronics/TestCrystal.py
+++ tests/Electronics/TestCrystal.py
@@ -7,6 +7,9 @@ from UliEngineering.EngineerIO import auto_format
 import numpy as np
 
 class TestCrystal(object):
-    def test_load_capacitory(self):
+    def test_load_capacitor(self):
         # Example from https://blog.adafruit.com/2012/01/24/choosing-the-right-crystal-and-caps-for-your-design/
         assert_equal(auto_format(load_capacitors, "6 pF", cpin="3 pF", cstray="2pF"), '5.00 pF')
+
+    def test_actual_load_capacitance(self):
+        assert_equal(auto_format(actual_load_capacitance, "5 pF", cpin="3 pF", cstray="2pF"), '6.00 pF')
