b55642395e5e32e36404f4bb8693d66ce12e07a9
Uli Köhler <ulikoehler@online.de>
2019-02-24 02:00:29 +0100

    Added correlation utilities

---
+++ UliEngineering/SignalProcessing/Correlation.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Functions for correlating a dataset with itself and other datasets
+Mainly built for 1D signal analysis.
+Might or might not work for higher-dimensional data.
+"""
+import numpy as np
+import scipy.signal
+
+__all__ = ["autocorrelate"]
+
+def autocorrelate(signal):
+    """
+    Auto-correlate a signal with itself.
+
+    Based on the fast FFT convolution using
+    scipy.signal.fftconvolve.
+    """
+    return scipy.signal.fftconvolve(signal, signal[::-1], mode='full')
---
+++ tests/SignalProcessing/TestCorrelation.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+from numpy.testing import assert_approx_equal, assert_allclose, assert_array_equal
+from nose.tools import assert_equal, assert_true, assert_false, raises, assert_less, assert_is_none, assert_raises, assert_is_instance
+from UliEngineering.SignalProcessing.Correlation import *
+from parameterized import parameterized
+import concurrent.futures
+import numpy as np
+import datetime
+
+class TestCorrelation(object):
+    def testAutocorrelation(self):
+        # Just test no exceptions
+        autocorrelate(np.random.random(1000))
