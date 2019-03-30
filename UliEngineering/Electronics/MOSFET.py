4ec563369cd45a1f429417668d60eff6439a95c4
Uli Köhler <ulikoehler@online.de>
2019-03-30 05:30:32 +0100

    Added MOSFET gate charge loss per cycle

--- UliEngineering/Electronics/MOSFET.py
+++ UliEngineering/Electronics/MOSFET.py
@@ -7,12 +7,12 @@ import numpy as np
 from UliEngineering.EngineerIO import normalize_numeric
 from UliEngineering.Units import Unit
 
-__all__ = ["mosfet_gate_charge_losses"]
+__all__ = ["mosfet_gate_charge_losses", "mosfet_gate_charge_loss_per_cycle"]
 
 def mosfet_gate_charge_losses(total_gate_charge, vsupply, frequency="100 kHz") -> Unit("W"):
     """
     Compute the gate charge loss of a MOSFET in a switch-mode
-    power-supply application.
+    power-supply application as a total power (integrated per second).
     
     Ref: 
     http://rohmfs.rohm.com/en/products/databook/applinote/ic/power/switching_regulator/power_loss_appli-e.pdf
@@ -28,7 +28,27 @@ def mosfet_gate_charge_losses(total_gate_charge, vsupply, frequency="100 kHz") -
     frequency: number or Engineer string
         The switching frequency in Hz
     """
+    frequency = normalize_numeric(frequency)
+    return mosfet_gate_charge_loss_per_cycle(total_gate_charge, vsupply) * frequency
+
+
+def mosfet_gate_charge_loss_per_cycle(total_gate_charge, vsupply) -> Unit("J"):
+    """
+    Compute the gate charge loss of a MOSFET in a switch-mode
+    power-supply application per switching cycle.
+    
+    Ref: 
+    http://rohmfs.rohm.com/en/products/databook/applinote/ic/power/switching_regulator/power_loss_appli-e.pdf
+
+    Parameters
+    ----------
+    total_gate_charge: number or Engineer string
+        The total gate charge in Coulomb.
+        For multiple MOSFETs such as in synchronous applications,
+        add their gate charges together.
+    vsupply: number or Engineer string
+        The gate driver supply voltage in Volts
+    """
     total_gate_charge = normalize_numeric(total_gate_charge)
     vsupply = normalize_numeric(vsupply)
-    frequency = normalize_numeric(frequency)
-    return total_gate_charge * vsupply * frequency
+    return total_gate_charge * vsupply
