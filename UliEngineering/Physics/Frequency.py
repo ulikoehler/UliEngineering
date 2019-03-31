dff7d657751a7a80e9a9e31ab644ba2ff9497ac1
Uli Köhler <ulikoehler@online.de>
2019-03-31 19:19:07 +0200

    Added period and frequency conversion

---
+++ UliEngineering/Physics/Frequency.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Utilities for frequencies
+"""
+from UliEngineering.EngineerIO import normalize_numeric
+from UliEngineering.Units import Unit
+import numpy as np
+from scipy.constants import zero_Celsius
+
+__all__ = ["frequency_to_period", "period_to_frequency"]
+
+
+def frequency_to_period(frequency) -> Unit("s"):
+    """
+    Compute the period associated with a frequency.
+
+    Parameters
+    ----------
+    frequency : number or Engineer string or NumPy array-like
+        The frequency in Hz
+    """
+    # Normalize inputs
+    frequency = normalize_numeric(frequency)
+    # Compute resistance
+    return 1./frequency
+
+def period_to_frequency(period) -> Unit("Hz"):
+    """
+    Compute the frequency associated with a period.
+
+    Parameters
+    ----------
+    period : number or Engineer string or NumPy array-like
+        The period in seconds
+    """
+    # Normalize inputs
+    period = normalize_numeric(period)
+    # Compute resistance
+    return 1./period
---
+++ tests/Physics/TestFrequency.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+from numpy.testing import assert_approx_equal, assert_allclose, assert_array_less
+from nose.tools import raises, assert_true, assert_equal
+from UliEngineering.Physics.Frequency import *
+from UliEngineering.Exceptions import *
+import functools
+import numpy as np
+
+class TestFrequencies(object):
+    def test_frequency_to_period(self):
+        assert_approx_equal(frequency_to_period(0.1), 10)
+        assert_approx_equal(frequency_to_period("0.1 Hz"), 10)
+        assert_approx_equal(frequency_to_period("10 Hz"), 0.1)
+        assert_approx_equal(frequency_to_period("10 kHz"), 0.1e-3)
+
+    def test_period_to_frequency(self):
+        assert_approx_equal(frequency_to_period(10), 0.1)
+        assert_approx_equal(frequency_to_period("10 s"), 0.1)
+        assert_approx_equal(frequency_to_period("10 ks"), 0.1e-3)
+        assert_approx_equal(frequency_to_period("1 ms"), 1e3)
