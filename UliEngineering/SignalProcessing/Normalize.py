f9f7b63a20f911ae90615318f21523bd63e76166
Uli Köhler <ulikoehler@online.de>
2019-04-24 05:06:06 +0200

    Added more normalization plus unit tests

--- UliEngineering/SignalProcessing/Normalize.py
+++ UliEngineering/SignalProcessing/Normalize.py
@@ -8,14 +8,21 @@ from collections import namedtuple
 import scipy.signal
 from .Utils import peak_to_peak
 
-__all__ = ["normalize_max", "center_to_zero", "normalize_minmax"]
+__all__ = ["normalize_max", "center_to_zero", "normalize_minmax", "normalize_plusminus_peak"]
 
 NormalizationResult = namedtuple("NormalizationResult", ["data", "factor", "offset"])
 
 def normalize_max(signal):
     """
     Normalize signal by dividing by its max value.
-    Does not perform any offset adjustment
+    Does not perform any offset adjustment.
+
+    This approach works well for data that is guaranteed to be
+    positive and if no offset adjustment is desired.
+
+    In case signal has a max of <= 0.0, signal is returned.
+    For a similar function that also limits the minimum value,
+    see normalize_plusminus_peak.
 
     Returns
     -------
@@ -24,14 +31,22 @@ def normalize_max(signal):
     Use .factor to access the factor that signal was divided by
     Use .offset to access the offset that was subtracted from signal
     """
+    if len(signal) == 0:
+        return NormalizationResult([], 1., 0.)
     mx = np.max(signal)
+    # Avoid divide by zero
+    if mx <= 0.:
+        return NormalizationResult(signal, 1., 0.)
     return NormalizationResult(signal / mx, mx, 0.)
 
 def normalize_minmax(signal):
     """
     Normalize signal by setting its lowest value
     to 0.0 and its highest value to 1.0,
-    keeping all other values
+    keeping all other values.
+
+    If signal consists of only zeros, no factor
+    normalization is applied.
 
     Returns
     -------
@@ -40,9 +55,13 @@ def normalize_minmax(signal):
     Use .factor to access the factor that signal was divided by
     Use .offset to access the offset that was subtracted from signal
     """
+    if len(signal) == 0:
+        return NormalizationResult([], 1., 0.)
     mi = np.min(signal)
     mx = np.max(signal)
     factor = mx - mi
+    if factor == 0.0:
+        factor = 1.0
     return NormalizationResult((signal - mi) / factor, factor, mi)
 
 def center_to_zero(signal):
@@ -59,3 +78,25 @@ def center_to_zero(signal):
     """
     mn = np.mean(signal)
     return NormalizationResult(signal - mn, 1., mn)
+
+
+def normalize_plusminus_peak(signal):
+    """
+    Center a signal to zero and normalize so that
+        - np.max(result) is <= 1.0
+        - np.min(result) is <= 1.0
+
+    Returns
+    -------
+    A NormalizationResult() object.
+    Use .data to access the data
+    Use .factor to access the factor that signal was divided by
+    Use .offset to access the offset that was subtracted from signal
+    """
+    norm_res = center_to_zero(signal)
+    mi = np.min(norm_res.data)
+    mx = np.max(norm_res.data)
+    factor = max(mi, mx)
+    return NormalizationResult(norm_res / factor, factor, norm_res.offset)
+
+
--- tests/SignalProcessing/TestNormalize.py
+++ tests/SignalProcessing/TestNormalize.py
@@ -18,3 +18,34 @@ class TestNormalize(object):
         assert_allclose(center_to_zero(np.asarray([0., 1.])).data, [-0.5, 0.5])
         assert_allclose(center_to_zero(np.asarray([0., 2.])).data, [-1., 1])
 
+    def test_normalize_max(self):
+        assert_allclose(normalize_max([]).data, [])
+        assert_allclose(normalize_max(np.asarray([0.])).data, [0.])
+        assert_allclose(normalize_max(np.asarray([1.])).data, [1.])
+        assert_allclose(normalize_max(np.asarray([-1., 1.])).data, [-1., 1.])
+        assert_allclose(normalize_max(np.asarray([-2., 2.])).data, [-1., 1.])
+        assert_allclose(normalize_max(np.asarray([0., 1.])).data, [0., 1.])
+        assert_allclose(normalize_max(np.asarray([-1., 0.])).data, [-1., 0.])
+        assert_allclose(normalize_max(np.asarray([-1., -2.])).data, [-1., -2.])
+        assert_allclose(normalize_max(np.asarray([0., 2.])).data, [0., 1])
+        assert_allclose(normalize_max(np.asarray([-10., 1.])).data, [-10., 1.])
+        assert_allclose(normalize_max(np.asarray([-10., 2.])).data, [-5., 1])
+
+    
+    def test_normalize_minmax(self):
+        assert_allclose(normalize_minmax([]).data, [])
+        assert_allclose(normalize_minmax(np.asarray([0.])).data, [0.])
+        assert_allclose(normalize_minmax(np.asarray([1.])).data, [0.])
+        assert_allclose(normalize_minmax(np.asarray([-1., 1.])).data, [0., 1.])
+        assert_allclose(normalize_minmax(np.asarray([-2., 2.])).data, [0., 1.])
+        assert_allclose(normalize_minmax(np.asarray([0., 1.])).data, [0., 1.])
+        assert_allclose(normalize_minmax(np.asarray([-1., 0.])).data, [0., 1.])
+        assert_allclose(normalize_minmax(np.asarray([-1., -2.])).data, [1., 0.])
+        assert_allclose(normalize_minmax(np.asarray([0., 2.])).data, [0., 1.])
+        assert_allclose(normalize_minmax(np.asarray([0., 0.])).data, [0., 0.])
+        assert_allclose(normalize_minmax(np.asarray([1., 1.])).data, [0., 0.])
+        assert_allclose(normalize_minmax(np.asarray([-10., 1.])).data, [0., 1.])
+        assert_allclose(normalize_minmax(np.asarray([-10., 2.])).data, [0., 1])
+        assert_allclose(normalize_minmax(np.asarray([1., 2., 3.])).data, [0., 0.5, 1.])
+        assert_allclose(normalize_minmax(np.asarray([3., 2., 1.])).data, [1., 0.5, 0.])
+
