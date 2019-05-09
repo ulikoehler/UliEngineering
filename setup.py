fba6ba0e5648d6aeed5c06c878b070cd17ce8e5c
Uli Köhler <ulikoehler@online.de>
2019-05-09 22:35:30 +0200

    Bump version to 0.2.2

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ if sys.version_info < (3, 0):
     sys.exit(1)
 
 setup(name='UliEngineering',
-      version='0.2.1',
+      version='0.2.2',
       description='Computational tools for electronics engineering',
       author='Uli Köhler',
       author_email='ukoehler@techoverflow.net',
