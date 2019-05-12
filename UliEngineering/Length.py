997a0a58857520bf3508e1fda1390916f99c0505
Uli Köhler <ulikoehler@online.de>
2019-05-12 04:34:52 +0200

    Added convert_length_to_meters

--- UliEngineering/Length.py
+++ UliEngineering/Length.py
@@ -7,7 +7,7 @@ import scipy.constants
 from .EngineerIO import EngineerIO
 from .Units import UnknownUnitInContextException, Unit
 
-__all__ = ["normalize_length"]
+__all__ = ["normalize_length", "convert_length_to_meters"]
 
 _length_factors = {
     "": 1., # Assumed. It's SI!
@@ -47,6 +47,14 @@ _length_factors = {
     'angstrom': scipy.constants.angstrom,
 }
 
+def convert_length_to_meters(value, unit, instance=EngineerIO.length_instance) -> Unit("m"):
+    """
+    Given a number or Engineer string (unit ignored) <value>
+    in <unit>, convert it to meters.
+    """
+    # Currently a hack, but doing it directly will not parse SI units
+    return normalize_length("{} {}".format(value, unit), instance=instance)
+
 def normalize_length(s, instance=EngineerIO.length_instance) -> Unit("m"):
     """
     Normalize a length to meters.
@@ -62,8 +70,6 @@ def normalize_length(s, instance=EngineerIO.length_instance) -> Unit("m"):
     - "1.2 M light years" => 1.135287656709696e+22
     - "9.15 kpc" => 2.8233949868947424e+17
     """
-    'mil','foot',
-    'ft', 'yd', 'yard', 'mile', 'pt', 'point', 'AU', 'ly', 'light year'
     value, unit = instance.normalize(s)
     if unit in _length_factors:
         return value * _length_factors[unit]
