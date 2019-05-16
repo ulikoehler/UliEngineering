a5cc337b8af8976e0c1a20ef2318125cd07e5f48
Uli Köhler <ulikoehler@online.de>
2019-05-16 06:23:05 +0200

    Added more date docs

--- UliEngineering/Utils/Date.py
+++ UliEngineering/Utils/Date.py
@@ -6,7 +6,11 @@ __all__ = ["Date", "all_dates_in_year", "number_of_days_in_month"]
 
 Date = namedtuple("Date", ["year", "month", "day"])
 
-def number_of_days_in_month(year=2019, month=2):
+def number_of_days_in_month(year=2019, month=1):
+    """
+    Returns the number of days in a month, e.g. 31 in January (month=1).
+    Takes into account leap days.
+    """
     return monthrange(year, month)[1]
 
 def all_dates_in_year(year=2019):
