62c81f1a12686d96de59885a737b2a1447a20d1e
Uli Köhler <ulikoehler@online.de>
2019-06-04 03:22:45 +0200

    Added power utilities

---
+++ UliEngineering/Electronics/Power.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+"""
+Utilities to compute the power of a device
+"""
+from UliEngineering.EngineerIO import normalize_numeric
+from UliEngineering.Units import Unit
+import numpy as np
+
+__all__ = ["current_by_power", "power_by_current_and_voltage"]
+
+def current_by_power(power="25 W", voltage="230 V") -> Unit("A"):
+    """
+    Given a device's power (or RMS power) and the voltage (or RMS voltage)
+    it runs on, compute how much current it will draw.
+    """
+    power = normalize_numeric(power)
+    voltage = normalize_numeric(voltage)
+    return power / voltage
+
+
+def power_by_current_and_voltage(current="1.0 A", voltage="230 V") -> Unit("W"):
+    """
+    Given a device's current (or RMS current) and the voltage (or RMS current)
+    it runs on, compute its power
+    """
+    current = normalize_numeric(current)
+    voltage = normalize_numeric(voltage)
+    return current * voltage
---
+++ tests/Electronics/TestPower.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+from numpy.testing import assert_approx_equal, assert_allclose, raises
+from nose.tools import assert_equal
+from UliEngineering.Electronics.Power import *
+from UliEngineering.EngineerIO import auto_format
+import numpy as np
+
+class TestPower(object):
+    def test_current_by_power(self):
+        assert_allclose(current_by_power(25), 25 / 230, atol=1e-15)
+        assert_allclose(current_by_power(25, 230), 25 / 230, atol=1e-15)
+        assert_allclose(current_by_power(25, 100), 25 / 100, atol=1e-15)
+        assert_allclose(current_by_power("25 W", "100 V"), 25 / 100, atol=1e-15)
+
+    def test_power_by_current_and_voltage(self):
+        assert_allclose(power_by_current_and_voltage(1, 10), 1*10, atol=1e-15)
+        assert_allclose(power_by_current_and_voltage(1, 230), 1 * 230, atol=1e-15)
+        assert_allclose(power_by_current_and_voltage(0.2, 230), 0.2 * 230, atol=1e-15)
+        assert_allclose(power_by_current_and_voltage(0.2), 0.2 * 230, atol=1e-15)
+        assert_allclose(power_by_current_and_voltage("0.2 A", "230 V"), 0.2 * 230, atol=1e-15)
+
