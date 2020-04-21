ae61adb71f4c7a689be30e9d6bb4354a7aa57db7
Uli Köhler <ukoehler@techoverflow.net>
2020-04-21 05:14:40 +0200

    Added hollow_cylinder_inner_radius_by_volume() + test

--- UliEngineering/Math/Geometry/Cylinder.py
+++ UliEngineering/Math/Geometry/Cylinder.py
@@ -6,10 +6,11 @@ Geometry functions for cylinders and hollow cylinders
 import math
 from UliEngineering.EngineerIO import normalize_numeric, Unit
 from .Circle import circle_area
+import numpy as np
 
 __all__ = [
     "cylinder_volume", "cylinder_side_surface_area", "cylinder_surface_area",
-    "hollow_cylinder_volume"
+    "hollow_cylinder_volume", "hollow_cylinder_inner_radius_by_volume"
 ]
 
 def cylinder_volume(radius, height) -> Unit("m³"):
@@ -41,3 +42,21 @@ def hollow_cylinder_volume(outer_radius, inner_radius, height) -> Unit("m³"):
     Compute the volume of a hollow cylinder by its height and the inner and outer radii
     """
     return cylinder_volume(outer_radius, height) - cylinder_volume(inner_radius, height)
+
+
+def hollow_cylinder_inner_radius_by_volume(outer_radius, volume, height) -> Unit("m"):
+    """
+    Given the outer radius, the height and the inner radius of a hollow cylinder,
+    compute the inner radius
+    """
+    outer_radius = normalize_numeric(outer_radius)
+    volume = normalize_numeric(volume)
+    height = normalize_numeric(height)
+    # Wolfram Alpha: solve V=(pi*o²*h)-(pi*i²*h) for i
+    term1 = np.pi*height*(outer_radius**2)-volume
+    # Due to rounding errors etc, term1 might become negative.
+    # This will lead to sqrt(-x) => NaN but we actually treat it as a zero result
+    if term1 < 0.:
+        return 0
+    # Default case
+    return np.sqrt(term1)/(np.sqrt(np.pi) * np.sqrt(height))
--- tests/Math/Geometry/TestCylinder.py
+++ tests/Math/Geometry/TestCylinder.py
@@ -65,3 +65,18 @@ class TestHollowCylinder(unittest.TestCase):
     def test_hollow_cylinder_volume(self, outer_radius, inner_radius, height, expected):
         self.assertAlmostEqual(hollow_cylinder_volume(outer_radius, inner_radius, height), expected, delta=.025)
         self.assertAlmostEqual(hollow_cylinder_volume(f"{outer_radius}", f"{inner_radius}", f"{height}"), expected, delta=.025)
+   
+
+    @parameterized.expand([
+        # Wolfram Alpha as a reference
+        # The following are not actually hollow (inner_radius=0.0)
+        (3.125, 0.0, 44.15, 1354.51),
+        # The following are actually hollow
+        (1.0, 0.5, 1.0, math.pi - 0.785398),
+        (4.0, 1.0, 2.5, 125.664-7.85398),
+        (3.125, 2.25, 44.15, 1354.51-702.175),
+    ])
+    def test_hollow_cylinder_volume(self, outer_radius, inner_radius, height, volume):
+        # NOTE: This uses the hollow_cylinder_volume() test cases except the ones that yield 0 volume
+        self.assertAlmostEqual(hollow_cylinder_inner_radius_by_volume(outer_radius, volume, height), inner_radius, delta=.025)
+        self.assertAlmostEqual(hollow_cylinder_inner_radius_by_volume(f"{outer_radius}", f"{volume}", f"{height}"), inner_radius, delta=.025)
