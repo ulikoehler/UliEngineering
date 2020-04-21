c865d6eda084c95c1a5deb8937f6799fbd57e816
Uli Köhler <ukoehler@techoverflow.net>
2020-04-21 04:30:16 +0200

    Added units for circle and cylinder

--- UliEngineering/Math/Geometry/Circle.py
+++ UliEngineering/Math/Geometry/Circle.py
@@ -4,20 +4,20 @@
 Circle geometry functions
 """
 import math
-from UliEngineering.EngineerIO import normalize_numeric
+from UliEngineering.EngineerIO import normalize_numeric, Unit
 
 __all__ = [
     "circle_area", "circle_circumference"
 ]
 
-def circle_area(radius):
+def circle_area(radius) -> Unit("m²"):
     """
     Compute the enclosed area of a circle from its radius
     """
     radius = normalize_numeric(radius)
     return math.pi * radius**2
 
-def circle_circumference(radius):
+def circle_circumference(radius) -> Unit("m"):
     """
     Compute the circumference of a circle from its radius
     """
--- UliEngineering/Math/Geometry/Cylinder.py
+++ UliEngineering/Math/Geometry/Cylinder.py
@@ -4,14 +4,14 @@
 Geometry functions for cylinders and hollow cylinders
 """
 import math
-from UliEngineering.EngineerIO import normalize_numeric
+from UliEngineering.EngineerIO import normalize_numeric, Unit
 from .Circle import circle_area
 
 __all__ = [
     "cylinder_volume", "cylinder_side_surface_area", "cylinder_surface_area"
 ]
 
-def cylinder_volume(radius, height):
+def cylinder_volume(radius, height) -> Unit("m³"):
     """
     Compute the volume of a cylinder by its radius and height
     """
@@ -19,7 +19,7 @@ def cylinder_volume(radius, height):
     height = normalize_numeric(height)
     return math.pi * (radius**2) * height
 
-def cylinder_side_surface_area(radius, height):
+def cylinder_side_surface_area(radius, height) -> Unit("m²"):
     """
     Compute the surface area of the side (also called ")
     """
@@ -27,7 +27,7 @@ def cylinder_side_surface_area(radius, height):
     height = normalize_numeric(height)
     return 2 * math.pi * radius * height
 
-def cylinder_surface_area(radius, height):
+def cylinder_surface_area(radius, height) -> Unit("m²"):
     """
     Compute the surface area (side + top + bottom)
     """
