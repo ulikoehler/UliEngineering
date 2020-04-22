b9b2eff4dfed5be558a1328a3b650a08c9f8026d
Uli Köhler <ukoehler@techoverflow.net>
2020-04-22 03:46:49 +0200

    Added rotating_liquid_pressure (untested)

--- UliEngineering/Physics/Rotation.py
+++ UliEngineering/Physics/Rotation.py
@@ -11,19 +11,19 @@ import scipy.constants
 __all__ = ["rpm_to_Hz", "rpm_to_rps", "hz_to_rpm", "angular_speed",
            "rotation_linear_speed", "centrifugal_force"]
 
-def rpm_to_Hz(rpm) -> Unit("Hz"):
+def rpm_to_Hz(rpm: Unit("rpm")) -> Unit("Hz"):
     """
     Compute the rotational speed in Hz given the rotational speed in rpm
     """
     rpm = normalize_numeric(rpm)
     return rpm / 60.
 
-def hz_to_rpm(hz) -> Unit("rpm"):
+def hz_to_rpm(speed: Unit("Hz")) -> Unit("rpm"):
     """
     Compute the rotational speed in rpm given the rotational speed in Hz
     """
-    hz = normalize_numeric(hz)
-    return hz * 60.
+    speed = normalize_numeric(speed)
+    return speed * 60.
 
 rpm_to_rps = rpm_to_Hz
 
@@ -48,3 +48,16 @@ def centrifugal_force(radius: Unit("m"), speed: Unit("Hz"), mass: Unit("g")) -> 
     radius = normalize_numeric(radius)
     mass = normalize_numeric(mass) / 1000.0 # mass needs to be Kilograms TODO Improve
     return mass * angular_speed(speed)**2 * radius
+
+def rotating_liquid_pressure(density: Unit("kg/m³"), speed: Unit("Hz"), radius: Unit("m")) -> Unit("N"):
+    """
+    Compute the pressure in a body of liquid (relative to the steady-state pressure)
+    The calculation does not include gravity.
+
+    Also see https://www.youtube.com/watch?v=kIH7wEq3H-M
+    Also see https://www.physicsforums.com/threads/pressure-of-a-rotating-bucket-of-liquid.38112/
+    """
+    density = normalize_numeric(density)
+    speed = normalize_numeric(speed)
+    radius = normalize_numeric(radius)
+    return  density * angular_speed(speed)**2 * radius**2
