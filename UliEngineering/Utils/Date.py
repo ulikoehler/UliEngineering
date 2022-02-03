7e02e8c502e0789a2f3110eab7fdcea3bd4ab3f4
Uli Köhler <ukoehler@techoverflow.net>
2022-02-03 01:02:41 +0100

    Added generate_days()

--- UliEngineering/Utils/Date.py
+++ UliEngineering/Utils/Date.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
 from collections import namedtuple
 from calendar import monthrange
+import numpy as np
 
-__all__ = ["Date", "all_dates_in_year", "number_of_days_in_month"]
+__all__ = ["Date", "all_dates_in_year", "number_of_days_in_month", "generate_days"]
 
 Date = namedtuple("Date", ["year", "month", "day"])
 
@@ -21,3 +22,24 @@ def all_dates_in_year(year=2019):
     for month in range(1, 13): # Month is always 1..12
         for day in range(1, number_of_days_in_month(year, month) + 1):
             yield Date(year, month, day)
+
+def generate_days(ndays, year=2022, month=1, day=1):
+    """
+    Generate an 1d array of [ndays] timestamps, starting at the given day,
+    each timestamp being exactly one day from the previous one.
+    The given date will be the first timestamp.
+
+    Returns a array of np.datetime64[us]
+
+    >>> generate_days(5, 2022, 1, 1)
+    ['2022-01-01T00:00:00.000000',
+     '2022-01-02T00:00:00.000000',
+     '2022-01-03T00:00:00.000000',
+     '2022-01-04T00:00:00.000000',
+     '2022-01-05T00:00:00.000000']
+    """
+    day_indexes = np.arange(ndays, dtype=np.int64) # 0, 1, ..., [ndays-1]
+    startdate = np.datetime64(f'{year:02d}-{month:02d}-{day:02d}T00:00:00.000000', 'us')
+    usec_per_day = int(1e6) * 86400 # 86.4k sec per day
+    usec_offsets = day_indexes * usec_per_day
+    return usec_offsets + startdate
--- tests/Utils/TestDate.py
+++ tests/Utils/TestDate.py
@@ -4,6 +4,8 @@ import os
 from subprocess import check_output
 from UliEngineering.Utils.Date import *
 import unittest
+from numpy.testing import assert_approx_equal, assert_allclose, assert_array_equal
+import numpy as np
 
 class TestDate(unittest.TestCase):
     def test_number_of_days_in_month(self):
@@ -15,3 +17,16 @@ class TestDate(unittest.TestCase):
         self.assertEqual(len(list(all_dates_in_year(2019))), 365)
         # Wolfram Alpha: number of days in 2020
         self.assertEqual(len(list(all_dates_in_year(2020))), 366)
+
+class TestGenerateDays(unittest.TestCase):
+    def test_empty(self):
+        assert_array_equal(generate_days(0), np.ndarray([], 'datetime64[us]'))
+    def test_simple(self):
+        assert_array_equal(generate_days(5, 2022, 1, 1),
+            np.asarray([
+                np.datetime64('2022-01-01T00:00:00.000000'),
+                np.datetime64('2022-01-02T00:00:00.000000'),
+                np.datetime64('2022-01-03T00:00:00.000000'),
+                np.datetime64('2022-01-04T00:00:00.000000'),
+                np.datetime64('2022-01-05T00:00:00.000000')
+            ], dtype='datetime64[us]'))
