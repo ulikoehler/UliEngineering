874e71430b0812ee90afd34d17191d13a75810eb
Uli Köhler <ukoehler@techoverflow.net>
2022-06-19 18:25:32 +0200

    Unit tests: Remove unused imports

--- tests/Utils/TestNumPy.py
+++ tests/Utils/TestNumPy.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-from numpy.testing import assert_approx_equal, assert_allclose, assert_array_equal
-from subprocess import check_output
+from numpy.testing import assert_allclose, assert_array_equal
 from UliEngineering.Utils.NumPy import *
 from parameterized import parameterized
 import numpy as np
