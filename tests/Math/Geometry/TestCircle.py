604823e5cb0c3b08cef5973755fb63e4eeb58464
Uli Köhler <ukoehler@techoverflow.net>
2022-06-19 18:46:32 +0200

    Fix unused imports (codacy)

--- tests/Math/Geometry/TestCircle.py
+++ tests/Math/Geometry/TestCircle.py
@@ -1,11 +1,9 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-from numpy.testing import assert_approx_equal, assert_allclose, assert_array_equal
+from numpy.testing import assert_approx_equal
 from UliEngineering.Math.Geometry.Circle import *
 from parameterized import parameterized
-import concurrent.futures
 import numpy as np
-import datetime
 import unittest
 
 
