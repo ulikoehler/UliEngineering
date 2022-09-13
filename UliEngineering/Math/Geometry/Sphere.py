b73b297dc4243944f80c68cccdc1420c435becd8
Uli Köhler <ukoehler@techoverflow.net>
2022-09-13 20:33:39 +0200

    Added sphere surface area functions

--- UliEngineering/Math/Geometry/Sphere.py
+++ UliEngineering/Math/Geometry/Sphere.py
@@ -3,7 +3,9 @@ import math
 from UliEngineering.EngineerIO import normalize_numeric, Unit
 
 __all__ = ["sphere_volume_by_radius",
-    "sphere_volume_by_diameter"]
+    "sphere_volume_by_diameter",
+    "sphere_surface_area_by_radius",
+    "sphere_surface_area_by_diameter"]
 
 def sphere_volume_by_radius(radius) -> Unit("m³"):
     """
@@ -18,3 +20,17 @@ def sphere_volume_by_diameter(diameter) -> Unit("m³"):
     """
     diameter = normalize_numeric(diameter)
     return sphere_volume_by_radius(diameter / 2.0)
+
+def sphere_surface_area_by_radius(radius) -> Unit("m²"):
+    """
+    Compute the surface area of a sphere of a given radius
+    """
+    radius = normalize_numeric(radius)
+    return 4. * math.pi * radius**2
+
+def sphere_surface_area_by_diameter(diameter) -> Unit("m²"):
+    """
+    Compute the surface area of a sphere of a given radius
+    """
+    diameter = normalize_numeric(diameter)
+    return sphere_surface_area_by_radius(diameter / 2.0)
--- tests/Math/Geometry/TestSphere.py
+++ tests/Math/Geometry/TestSphere.py
@@ -23,3 +23,18 @@ class TestSphere(unittest.TestCase):
         assert_approx_equal(sphere_volume_by_radius(f"{radius}"), volume)
         assert_approx_equal(sphere_volume_by_diameter(radius*2), volume)
 
+    @parameterized.expand([
+        (0.0, ),
+        (1.0, ),
+        (4.0, ),
+        (3.125, ),
+        (55.55, ),
+    ])
+    def test_sphere_surface_area(self, radius):
+        area = 4*np.pi*(radius**2)
+        assert_approx_equal(sphere_surface_area_by_radius(radius), area)
+        assert_approx_equal(sphere_surface_area_by_diameter(radius*2), area)
+        assert_approx_equal(sphere_surface_area_by_radius(f"{radius}"), area)
+        assert_approx_equal(sphere_surface_area_by_diameter(radius*2), area)
+
+
