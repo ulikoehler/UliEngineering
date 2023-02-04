4b3bfe3d11a48bd614d3e41a9b6e8cfe2b7e6e8f
Uli Köhler <ukoehler@techoverflow.net>
2023-02-04 02:11:29 +0100

    Add more docs for larmor_frequency()

--- UliEngineering/Physics/MagneticResonance.py
+++ UliEngineering/Physics/MagneticResonance.py
@@ -14,6 +14,11 @@ def larmor_frequency(b0, nucleus_larmor_frequency=NucleusLarmorFrequency.H1) -> 
     """
     Get the magnetic resonance frequency (larmor frequency)
     for a given nucleus in a given magnetic field strength B0
+    
+    Note that the frequency is given in Hz, not in MHz!
+    
+    :param b0: Magnetic field strength in Tesla
+    :param nucleus_larmor_frequency: Larmor frequency of the nucleus in MHz/T
     """
     b0 = normalize_numeric(b0)
-    return b0 * nucleus_larmor_frequency
+    return b0 * (nucleus_larmor_frequency * 1e6) # MHz/T -> Hz/T
