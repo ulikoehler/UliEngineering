14191b7b6097c07c766b05c8765f71f29f1bda6c
Uli Köhler <ukoehler@techoverflow.net>
2023-02-04 02:13:28 +0100

    Fix larmor_frequency unit test

--- tests/Physics/TestMagneticResonance.py
+++ tests/Physics/TestMagneticResonance.py
@@ -8,7 +8,7 @@ import unittest
 class TestLarmorFrequency(unittest.TestCase):
     def test_larmor_frequency_h1(self):
         self.assertAlmostEqual(larmor_frequency(0., nucleus_larmor_frequency=NucleusLarmorFrequency.H1), 0)
-        self.assertAlmostEqual(larmor_frequency(1., nucleus_larmor_frequency=NucleusLarmorFrequency.H1), 42.57638474)
-        self.assertAlmostEqual(larmor_frequency(2.2, nucleus_larmor_frequency=NucleusLarmorFrequency.H1), 2.2*42.57638474)
+        self.assertAlmostEqual(larmor_frequency(1., nucleus_larmor_frequency=NucleusLarmorFrequency.H1), 42.57638474e6)
+        self.assertAlmostEqual(larmor_frequency(2.2, nucleus_larmor_frequency=NucleusLarmorFrequency.H1), 2.2*42.57638474e6)
         # H1 hould be the standard value
         self.assertAlmostEqual(larmor_frequency(0.), 0)
