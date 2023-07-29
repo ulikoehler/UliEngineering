ee1b5ff26b407fa14fed5bf81361f3bed53a9d0a
Uli Köhler <ukoehler@techoverflow.net>
2023-07-29 04:50:36 +0200

    Increase decibel test coverage

--- tests/Math/TestDecibel.py
+++ tests/Math/TestDecibel.py
@@ -50,10 +50,17 @@ class TestDecibel(unittest.TestCase):
         assert_allclose(-9, value_to_dB(0.25, 2, factor=dBFactor.Power), 0.05)
         # Test string
         assert_allclose(6, value_to_dB("4 V", "2 V", factor=dBFactor.Field), 0.05)
-        
-    def test_ratio_to_dB_infinite(self):
-        # Test negative
-        self.assertEqual(-np.inf, value_to_dB(0, ))
-        self.assertEqual(-np.inf, value_to_dB(0, factor=dBFactor.Power))
-        self.assertEqual(-np.inf, value_to_dB(-5))
-        self.assertEqual(-np.inf, value_to_dB(-5, factor=dBFactor.Power))
+    
+    def test_dB_to_ratio(self):
+        self.assertAlmostEqual(dB_to_ratio(0), 1.0)
+        self.assertAlmostEqual(dB_to_ratio(20), 10.0)
+        self.assertAlmostEqual(dB_to_ratio(-20), 0.1)
+        self.assertAlmostEqual(dB_to_ratio(20, factor=dBFactor.Power), 100.0)
+        self.assertAlmostEqual(dB_to_ratio(-20, factor=dBFactor.Power), 0.01)
+
+    def test_dB_to_value(self):
+        self.assertAlmostEqual(dB_to_value(0), 1.0)
+        self.assertAlmostEqual(dB_to_value(20), 10.0)
+        self.assertAlmostEqual(dB_to_value(-20), 0.1)
+        self.assertAlmostEqual(dB_to_value(20, v0=100), 1000.0)
+        self.assertAlmostEqual(dB_to_value(-20, v0=100), 10.0)
