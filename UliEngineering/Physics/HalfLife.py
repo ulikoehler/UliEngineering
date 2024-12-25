d5b3abd52e2dce27012ee885c684ddd446c1152a
Uli Köhler <ukoehler@techoverflow.net>
2024-12-25 22:28:25 +0100

    Started to add half life code

---
+++ UliEngineering/Physics/HalfLife.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# # -*- coding: utf-8 -*-
+import numpy as np
+
+def half_lifes_passed():
+    """
+    Compute the number of half-lifes that have passed
+    within a 
+    """
+    return np.log(2) / np.log(1.5)
