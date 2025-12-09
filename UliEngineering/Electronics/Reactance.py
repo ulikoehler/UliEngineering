f44d182b6cec1302d947187833678883a72f3695
Uli Köhler <ukoehler@techoverflow.net>
2025-12-09 03:42:33 +0100

    Add inverse functions for inductive and capacitive reactance

--- UliEngineering/Electronics/Reactance.py
+++ UliEngineering/Electronics/Reactance.py
@@ -9,7 +9,12 @@ import numpy as np
 
 from UliEngineering.EngineerIO.Decorators import normalize_numeric_args, returns_unit
 
-__all__ = ["capacitive_reactance", "inductive_reactance"]
+__all__ = [
+    "capacitive_reactance",
+    "inductive_reactance",
+    "inductance_from_reactance",
+    "capacitance_from_reactance",
+]
 
 @normalize_numeric_args
 @returns_unit("Ω")
@@ -27,3 +32,25 @@ def inductive_reactance(l, f=1000.0):
     Compute the inductive reactance for a given inductance and frequency.
     """
     return 2 * np.pi * f * l
+
+@normalize_numeric_args
+@returns_unit("H")
+def inductance_from_reactance(x, f=1000.0):
+    """
+    Compute the inductance (H) from an inductive reactance (Ω) at a given
+    frequency f (Hz).
+
+    Formula: X_L = 2 * pi * f * L  =>  L = X_L / (2 * pi * f)
+    """
+    return x / (2 * np.pi * f)
+
+@normalize_numeric_args
+@returns_unit("F")
+def capacitance_from_reactance(x, f=1000.0):
+    """
+    Compute the capacitance (F) from a capacitive reactance (Ω) at a given
+    frequency f (Hz).
+
+    Formula: X_C = 1 / (2 * pi * f * C)  =>  C = 1 / (2 * pi * f * X_C)
+    """
+    return 1.0 / (2 * np.pi * f * x)
--- tests/Electronics/TestReactance.py
+++ tests/Electronics/TestReactance.py
@@ -21,3 +21,56 @@ class TestNoiseDensity(unittest.TestCase):
     def test_numpy_arrays(self):
         l = np.asarray([100e-6, 200e-6])
         assert_allclose(inductive_reactance(l, 3.2e6), [2010.6193, 4021.23859659])
+
+    def test_inverse_inductive(self):
+        # float inputs
+        l = 100e-6
+        f = 3.2e6
+        x = inductive_reactance(l, f)
+        l2 = inductance_from_reactance(x, f)
+        assert_approx_equal(l2, l)
+
+        # string inputs and formatting
+        l_str = "100 µH"
+        x = inductive_reactance(l_str, "3.2 MHz")
+        self.assertAlmostEqual(x, 2010.619, places=3)
+        l_back = inductance_from_reactance(x, "3.2 MHz")
+        assert_approx_equal(l_back, 100e-6)
+        af = auto_format(inductance_from_reactance, "2.01 kΩ", "3.2 MHz")
+        # allow different float formatting like '100' or '100.0' etc.
+        val, unit = af.split()
+        self.assertAlmostEqual(float(val), 100.0, places=3)
+        self.assertEqual(unit, "µH")
+
+    def test_inverse_capacitive(self):
+        # float input
+        c = 100e-12
+        f = 3.2e6
+        x = capacitive_reactance(c, f)
+        c2 = capacitance_from_reactance(x, f)
+        assert_allclose(c2, c)
+
+        # string inputs and formatting
+        c_str = "100 pF"
+        x = capacitive_reactance(c_str, "3.2 MHz")
+        self.assertAlmostEqual(x, 497.3592, places=3)
+        c_back = capacitance_from_reactance(x, "3.2 MHz")
+        assert_approx_equal(c_back, 100e-12)
+        af = auto_format(capacitance_from_reactance, "497 Ω", "3.2 MHz")
+        val, unit = af.split()
+        self.assertAlmostEqual(float(val), 100.0, places=3)
+        self.assertEqual(unit, "pF")
+
+    def test_inverse_numpy_arrays(self):
+        # Inductive: arrays
+        l = np.asarray([10e-6, 100e-6])
+        f = 1e6
+        x = inductive_reactance(l, f)
+        l_back = inductance_from_reactance(x, f)
+        assert_allclose(l_back, l)
+
+        # Capacitive: arrays
+        c = np.asarray([10e-12, 100e-12])
+        x = capacitive_reactance(c, f)
+        c_back = capacitance_from_reactance(x, f)
+        assert_allclose(c_back, c)
