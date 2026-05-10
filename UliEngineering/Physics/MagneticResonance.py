5179c82631778728fe7d92f62cf9fcf1f553535a
Uli Köhler <ukoehler@techoverflow.net>
2026-05-10 04:17:25 +0200

    Add normalize functions and Annotated types to MagneticResonance module
    - Add normalize_magnetic_field function with magnetic field units (T, Tesla, tesla, mT, µT, G, Gauss, gauss)
    - Add MagneticFieldTesla type annotation
    - Import FrequencyHz and normalize_frequency from Frequency module
    - Update function signature to use new types
    - Manually call normalize function in function body
    - Add comprehensive unit tests for magnetic field units
    - Test type annotation existence

--- UliEngineering/Physics/MagneticResonance.py
+++ UliEngineering/Physics/MagneticResonance.py
@@ -1,10 +1,17 @@
 #!/usr/bin/env python3
-from UliEngineering.EngineerIO.Decorators import normalize_numeric_args, returns_unit
+from typing import Annotated
+
+from UliEngineering.EngineerIO.Decorators import returns_unit
+from UliEngineering.EngineerIO.Types import NormalizableArgument, NormalizedComputable
+from UliEngineering.Physics.Frequency import FrequencyHz, normalize_frequency
+from ._normalize import normalize_with_known_units
 import scipy.constants
 
 __all__ = [
     "NucleusLarmorFrequency",
-    "larmor_frequency"
+    "larmor_frequency",
+    "normalize_magnetic_field", "MagneticFieldTesla",
+    "normalize_frequency", "FrequencyHz"
 ]
 
 # Nucleus Larmor frequencies in Hz
@@ -14,9 +21,13 @@ class NucleusLarmorFrequency:
     H1 = scipy.constants.physical_constants['shielded proton gyromag. ratio in MHz/T'][0]
     He3 = scipy.constants.physical_constants['shielded helion gyromag. ratio in MHz/T'][0]
 
+def normalize_magnetic_field(field: NormalizableArgument) -> NormalizedComputable:
+    return normalize_with_known_units(field, {"T": 1.0, "Tesla": 1.0, "tesla": 1.0, "mT": 1e-3, "µT": 1e-6, "G": 1e-4, "Gauss": 1e-4, "gauss": 1e-4}, quantity_name="magnetic field")
+
+MagneticFieldTesla = Annotated[NormalizedComputable, normalize_magnetic_field]
+
 @returns_unit("Hz")
-@normalize_numeric_args
-def larmor_frequency(b0, nucleus_larmor_frequency=NucleusLarmorFrequency.H1):
+def larmor_frequency(b0: MagneticFieldTesla, nucleus_larmor_frequency=NucleusLarmorFrequency.H1):
     """
     Get the magnetic resonance frequency (larmor frequency)
     for a given nucleus in a given magnetic field strength B0.
@@ -26,4 +37,5 @@ def larmor_frequency(b0, nucleus_larmor_frequency=NucleusLarmorFrequency.H1):
     :param b0: Magnetic field strength in Tesla
     :param nucleus_larmor_frequency: Larmor frequency of the nucleus in MHz/T
     """
+    b0 = normalize_magnetic_field(b0)
     return b0 * (nucleus_larmor_frequency * 1e6) # MHz/T -> Hz/T
--- tests/Physics/TestMagneticResonance.py
+++ tests/Physics/TestMagneticResonance.py
@@ -1,6 +1,11 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-from UliEngineering.Physics.MagneticResonance import larmor_frequency, NucleusLarmorFrequency
+from numpy.testing import assert_approx_equal
+from UliEngineering.Physics.MagneticResonance import (
+    larmor_frequency, NucleusLarmorFrequency,
+    normalize_magnetic_field, MagneticFieldTesla,
+    normalize_frequency, FrequencyHz
+)
 import unittest
 
 class TestLarmorFrequency(unittest.TestCase):
@@ -8,5 +13,39 @@ class TestLarmorFrequency(unittest.TestCase):
         self.assertAlmostEqual(larmor_frequency(0., nucleus_larmor_frequency=NucleusLarmorFrequency.H1), 0)
         self.assertAlmostEqual(larmor_frequency(1., nucleus_larmor_frequency=NucleusLarmorFrequency.H1), 42.57638543e6, places=6)
         self.assertAlmostEqual(larmor_frequency(2.2, nucleus_larmor_frequency=NucleusLarmorFrequency.H1), 2.2*42.57638543e6)
-        # H1 hould be the standard value
+        # H1 should be the standard value
         self.assertAlmostEqual(larmor_frequency(0.), 0)
+
+    def test_type_annotations_exist(self):
+        """Test that the new type annotations are available"""
+        self.assertIsNotNone(MagneticFieldTesla)
+        self.assertIsNotNone(FrequencyHz)
+
+    def test_normalize_magnetic_field_various_units(self):
+        """Test normalize_magnetic_field with various unit inputs"""
+        test_cases = [
+            ("1 T", 1.0),
+            ("1 Tesla", 1.0),
+            ("1 tesla", 1.0),
+            ("1 mT", 1e-3),
+            ("1 µT", 1e-6),
+            ("1 G", 1e-4),
+            ("1 Gauss", 1e-4),
+            ("1 gauss", 1e-4),
+        ]
+        for input_val, expected in test_cases:
+            with self.subTest(input=input_val):
+                result = normalize_magnetic_field(input_val)
+                assert_approx_equal(result, expected)
+
+    def test_larmor_frequency_various_units(self):
+        """Test larmor_frequency with various magnetic field units"""
+        # Test with different magnetic field units
+        f1 = larmor_frequency("1 T", nucleus_larmor_frequency=NucleusLarmorFrequency.H1)
+        f2 = larmor_frequency("1 Tesla", nucleus_larmor_frequency=NucleusLarmorFrequency.H1)
+        assert_approx_equal(f1, f2)
+        
+        # Test with mT
+        f3 = larmor_frequency("1000 mT", nucleus_larmor_frequency=NucleusLarmorFrequency.H1)
+        f4 = larmor_frequency("1 T", nucleus_larmor_frequency=NucleusLarmorFrequency.H1)
+        assert_approx_equal(f3, f4)
