67f108c6d61f2bb2f9bfa1b105a595bb9df01d8b
Uli Köhler <ukoehler@techoverflow.net>
2026-05-10 05:52:47 +0200

    Add Annotated types to Optoelectronics/MPPC module
    - Import normalize_numeric, NormalizableArgument from EngineerIO
    - Import normalize_capacitance, CapacitanceFarad from Electronics.Capacitors
    - Update function signatures to use CapacitanceFarad and NormalizableArgument
    - Remove @normalize_numeric_args decorator
    - Add conditional normalization calls for string input support

--- UliEngineering/Optoelectronics/MPPC.py
+++ UliEngineering/Optoelectronics/MPPC.py
@@ -2,15 +2,17 @@
 # -*- coding: utf-8 -*-
 """Functions for Multi-Pixel photon counters (MPPC utilities)."""
 
-from UliEngineering.EngineerIO.Decorators import normalize_numeric_args, returns_unit
+from UliEngineering.EngineerIO.Decorators import returns_unit
+from UliEngineering.EngineerIO import normalize_numeric
+from UliEngineering.EngineerIO.Types import NormalizableArgument
+from .Electronics.Capacitors import normalize_capacitance, CapacitanceFarad
 
 __all__ = [
     "pixel_capacitance_from_terminal_capacitance"
 ]
 
-@normalize_numeric_args
 @returns_unit("F")
-def pixel_capacitance_from_terminal_capacitance(terminal_capacitance="900pF", npixels=14331):
+def pixel_capacitance_from_terminal_capacitance(terminal_capacitance: CapacitanceFarad = "900pF", npixels: NormalizableArgument = 14331):
     """
     Estimate a MPPC's individual pixel's capacitance from the terminal capacitance.
     
@@ -20,4 +22,6 @@ def pixel_capacitance_from_terminal_capacitance(terminal_capacitance="900pF", np
     
     This method is outlined as alternate method [by Hamamatsu](https://hub.hamamatsu.com/us/en/technical-notes/mppc-sipms/a-technical-guide-to-silicon-photomutlipliers-MPPC-Section-3.html)
     """
+    terminal_capacitance = normalize_capacitance(terminal_capacitance) if isinstance(terminal_capacitance, str) else terminal_capacitance
+    npixels = normalize_numeric(npixels) if isinstance(npixels, str) else npixels
     return terminal_capacitance / npixels
